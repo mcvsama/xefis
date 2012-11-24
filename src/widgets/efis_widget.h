ccd3350f51146f714ecf9bfc9ce0cd9ba352f5db
Michał Gawron <michal@gawron.name>
2012-11-24 18:48:47 +0100

    Move common code to base class: InstrumentWidget.

--- src/widgets/efis_widget.h
+++ src/widgets/efis_widget.h
@@ -26,10 +26,11 @@
 
 // Xefis:
 #include <xefis/config/all.h>
+#include <xefis/core/instrument_widget.h>
 #include <xefis/utility/text_painter.h>
 
 
-class EFISWidget: public QWidget
+class EFISWidget: public Xefis::InstrumentWidget
 {
 	Q_OBJECT
 
@@ -584,9 +585,6 @@ class EFISWidget: public QWidget
 	paintEvent (QPaintEvent*) override;
 
 	void
-	resizeEvent (QResizeEvent*) override;
-
-	void
 	paint_center_cross (QPainter&);
 
 	void
@@ -596,48 +594,11 @@ class EFISWidget: public QWidget
 	paint_input_alert (QPainter&);
 
   private:
-	/**
-	 * Return min (width(), height());
-	 */
-	float
-	wh() const;
-
-	QPen
-	get_pen (QColor const& color, float width);
-
-	float
-	pen_width (float scale = 1.0f) const;
-
-	float
-	font_size (float scale = 1.0f) const;
-
-	int
-	get_digit_width (QFont&) const;
-
-	void
-	update_fonts();
-
-  private:
-	QFont				_font_10_bold;
-	QFont				_font_13_bold;
-	QFont				_font_16_bold;
-	QFont				_font_20_bold;
-	float				_font_10_digit_width;
-	float				_font_13_digit_width;
-	float				_font_16_digit_width;
-	float				_font_20_digit_width;
-	float				_font_10_digit_height;
-	float				_font_13_digit_height;
-	float				_font_16_digit_height;
-	float				_font_20_digit_height;
 	QColor				_sky_color;
 	QColor				_ground_color;
 	QColor				_ladder_color;
 	QColor				_ladder_border_color;
-	QColor				_autopilot_color;
-	QColor				_navigation_color;
 	QTransform			_center_transform;
-	QFont				_font;
 	Degrees				_fov						= 120.f;
 	bool				_input_alert_visible		= false;
 	TextPainter::Cache	_text_painter_cache;
@@ -682,9 +643,6 @@ class EFISWidget: public QWidget
 	bool				_at_speed_visible			= false;
 	SpeedBugs			_speed_bugs;
 	AltitudeBugs		_altitude_bugs;
-
-	static const char	DIGITS[];
-	static const char*	MINUS_SIGN;
 };
 
 
@@ -1209,34 +1167,6 @@ inline void
 EFISWidget::set_input_alert_visibility (bool visible)
 {
 	_input_alert_visible = visible;
-}
-
-
-inline float
-EFISWidget::wh() const
-{
-	return std::min (0.8f * width(), 1.f * height());
-}
-
-
-inline QPen
-EFISWidget::get_pen (QColor const& color, float width)
-{
-	return QPen (color, pen_width (width), Qt::SolidLine, Qt::SquareCap, Qt::MiterJoin);
-}
-
-
-inline float
-EFISWidget::pen_width (float scale) const
-{
-	return std::max (0.f, scale * wh() / 325.f);
-}
-
-
-inline float
-EFISWidget::font_size (float scale) const
-{
-	return std::max (1.f, scale * wh() / 375.f);
 }
 
 #endif
