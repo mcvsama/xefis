cea7beb7de513f8246313273bcfe3ded7c967b8e
Michał Gawron <mgawron@opera.com>
2012-12-14 17:36:29 +0100

    Module manager.

---
+++ src/xefis/core/module_manager.h
@@ -0,0 +1,50 @@
+/* vim:ts=4
+ *
+ * Copyleft 2008…2012  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__COMPONENTS__MODULE_MANAGER_H__INCLUDED
+#define XEFIS__COMPONENTS__MODULE_MANAGER_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <set>
+
+// Xefis:
+#include <xefis/config/all.h>
+
+
+namespace Xefis {
+
+class Module;
+
+class ModuleManager
+{
+	typedef std::set<Module*> Modules;
+
+  public:
+	virtual ~ModuleManager();
+
+	/**
+	 * Add module. The module will be deleted
+	 * when ModuleManager is deleted.
+	 */
+	void
+	add_module (Module*);
+
+  private:
+	Modules	_modules;
+};
+
+} // namespace Xefis
+
+#endif
+
