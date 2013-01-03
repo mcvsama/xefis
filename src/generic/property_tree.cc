033691d787cb462ec68af091b4d084bfee41e5ad
Michał Gawron <michal@gawron.name>
2013-01-03 20:19:22 +0100

    Make PropertyTreeWidget a loadable module.

---
+++ src/generic/property_tree.cc
@@ -0,0 +1,39 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtCore/QTimer>
+#include <QtGui/QLayout>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/utility/qdom.h>
+
+// Local:
+#include "property_tree.h"
+
+
+PropertyTree::PropertyTree (QDomElement const&, QWidget* parent):
+	Instrument (parent)
+{
+	_widget = new Xefis::PropertyTreeWidget (Xefis::PropertyStorage::root(), this);
+
+	QVBoxLayout* layout = new QVBoxLayout (this);
+	layout->setMargin (0);
+	layout->setSpacing (0);
+	layout->addWidget (_widget);
+}
+
