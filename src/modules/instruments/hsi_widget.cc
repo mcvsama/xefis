801347f1efbe45fdcba14a68b3c3606d84eb3375
Michał Gawron <michal@gawron.name>
2013-02-09 22:15:04 +0100

    Fix painting range ticks on HSI.

--- src/modules/instruments/hsi_widget.cc
+++ src/modules/instruments/hsi_widget.cc
@@ -330,7 +330,11 @@ HSIWidget::paint_track (QPainter& painter, TextPainter& text_painter)
 
 		auto paint_range_tick = [&] (float ratio, bool draw_text) -> void
 		{
-			Length range = 1_nm * std::round (((10.f * ratio * _range) / 10.f).nm());
+			Length range;
+			if (ratio == 0.5)
+				range = 1_nm * std::round (((10.f * ratio * _range) / 10.f).nm());
+			else
+				range = ratio * _range;
 			float range_tick_vpx = nm_to_px (range);
 			float range_tick_hpx = 0.1f * _q;
 			QString half_range_str = QString ("%1").arg (range.nm(), 0, 'f', 0);
