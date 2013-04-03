b5ed85f1a411a6d695082df920cbc201411b2416
Michał Gawron <michal@gawron.name>
2013-04-03 21:12:12 +0200

    Migrate FMS to use SIProperties.

--- src/modules/systems/fms.h
+++ src/modules/systems/fms.h
@@ -77,118 +77,118 @@ class FlightManagementSystem: public Xefis::Module
   private:
 	Time					_now;
 	// [0] - new, [2] - old
-	std::array<Position, 3>	_positions;
-	std::array<Position, 3>	_ac1_positions;
-	std::array<Position, 3>	_ac2_positions;
-	Length					_alt_amsl_prev						= 0_ft;
-	Time					_alt_amsl_time						= 0_s;
-	Speed					_computed_climb_rate				= 0_fpm;
-	int						_positions_valid					= 0;
-	Xefis::Smoother<double>	_track_vertical_smoother			= 50.0; // TODO make fps independent
-	Xefis::Smoother<double>	_track_true_heading_smoother		= 100.0; // TODO make fps independent
-	Xefis::Smoother<double>	_wind_direction_smoother			= 2000.0; // TODO make fps independent
-	Xefis::Smoother<double>	_ground_speed_smoother				= 200.0; // TODO make fps independent
-	Xefis::Smoother<double>	_climb_rate_smoother				= 400.0; // TODO make fps independent
-	Xefis::Smoother<double>	_pressure_alt_smoother				= 100.0; // TODO make fps independent
+	std::array<Position, 3>		_positions;
+	std::array<Position, 3>		_ac1_positions;
+	std::array<Position, 3>		_ac2_positions;
+	Length						_alt_amsl_prev						= 0_ft;
+	Time						_alt_amsl_time						= 0_s;
+	Speed						_computed_climb_rate				= 0_fpm;
+	int							_positions_valid					= 0;
+	Xefis::Smoother<double>		_track_vertical_smoother			= 50.0; // TODO make fps independent
+	Xefis::Smoother<double>		_track_true_heading_smoother		= 100.0; // TODO make fps independent
+	Xefis::Smoother<double>		_wind_direction_smoother			= 2000.0; // TODO make fps independent
+	Xefis::Smoother<double>		_ground_speed_smoother				= 200.0; // TODO make fps independent
+	Xefis::Smoother<double>		_climb_rate_smoother				= 400.0; // TODO make fps independent
+	Xefis::Smoother<double>		_pressure_alt_smoother				= 100.0; // TODO make fps independent
 
 	// Input parameters:
-	Xefis::PropertyFloat	_default_airplane_weight_kg;
-	Xefis::PropertyFloat	_actual_airplane_weight_kg;
-	Xefis::PropertyFloat	_low_speed_roll_angle_deg;
-	Xefis::PropertyFloat	_v_a_default_kt;
-	Xefis::PropertyFloat	_v_no_default_kt;
-	Xefis::PropertyFloat	_v_ne_kt;
-	Xefis::PropertyFloat	_v_s_kt;
-	Xefis::PropertyFloat	_v_s0_kt;
-	Xefis::PropertyFloat	_v_at_kt;
-	Xefis::PropertyFloat	_v_fe_kt;
-	Xefis::PropertyFloat	_v_le_kt;
-	Xefis::PropertyFloat	_v_o_kt;
-	Xefis::PropertyFloat	_v_be_kt;
-	Xefis::PropertyFloat	_v_bg_kt;
-	Xefis::PropertyFloat	_v_br_kt;
-	Xefis::PropertyString	_flaps_configuration_properties_path;
-	Xefis::PropertyBoolean	_gear_down;
-	Xefis::PropertyFloat	_static_pressure_inhg;
-	Xefis::PropertyFloat	_qnh_pressure_inhg;
-	Xefis::PropertyFloat	_backup_amsl_ft;
-	Xefis::PropertyFloat	_ias_kt;
-	Xefis::PropertyFloat	_outside_air_temperature_k;
+	Xefis::PropertyFloat		_default_airplane_weight_kg;
+	Xefis::PropertyFloat		_actual_airplane_weight_kg;
+	Xefis::PropertyAngle		_low_speed_roll_angle;
+	Xefis::PropertySpeed		_v_a_default;
+	Xefis::PropertySpeed		_v_no_default;
+	Xefis::PropertySpeed		_v_ne;
+	Xefis::PropertySpeed		_v_s;
+	Xefis::PropertySpeed		_v_s0;
+	Xefis::PropertySpeed		_v_at;
+	Xefis::PropertySpeed		_v_fe;
+	Xefis::PropertySpeed		_v_le;
+	Xefis::PropertySpeed		_v_o;
+	Xefis::PropertySpeed		_v_be;
+	Xefis::PropertySpeed		_v_bg;
+	Xefis::PropertySpeed		_v_br;
+	Xefis::PropertyString		_flaps_configuration_properties_path;
+	Xefis::PropertyBoolean		_gear_down;
+	Xefis::PropertyPressure		_static_pressure;
+	Xefis::PropertyPressure		_qnh_pressure;
+	Xefis::PropertyLength		_backup_amsl;
+	Xefis::PropertySpeed		_ias;
+	Xefis::PropertyFloat		_outside_air_temperature_k;
 	// Input IMU:
-	Xefis::PropertyFloat	_imu_pitch_deg;
-	Xefis::PropertyFloat	_imu_roll_deg;
-	Xefis::PropertyFloat	_imu_magnetic_heading_deg;
-	Xefis::PropertyFloat	_imu_magnetic_heading_accuracy_deg;
+	Xefis::PropertyAngle		_imu_pitch;
+	Xefis::PropertyAngle		_imu_roll;
+	Xefis::PropertyAngle		_imu_magnetic_heading;
+	Xefis::PropertyAngle		_imu_magnetic_heading_accuracy;
 	// Input GPS:
-	Xefis::PropertyFloat	_gps_longitude_deg;
-	Xefis::PropertyFloat	_gps_latitude_deg;
-	Xefis::PropertyFloat	_gps_altitude_amsl_ft;
-	Xefis::PropertyFloat	_gps_accuracy_nm;
-	Xefis::PropertyFloat	_gps_timestamp_s;
+	Xefis::PropertyAngle		_gps_longitude;
+	Xefis::PropertyAngle		_gps_latitude;
+	Xefis::PropertyLength		_gps_altitude_amsl;
+	Xefis::PropertyLength		_gps_accuracy;
+	Xefis::PropertyTime			_gps_timestamp;
 	// Input INS (Inertial Navigation System):
-	Xefis::PropertyFloat	_ins_longitude_deg;
-	Xefis::PropertyFloat	_ins_latitude_deg;
-	Xefis::PropertyFloat	_ins_altitude_amsl_ft;
-	Xefis::PropertyFloat	_ins_accuracy_nm;
-	Xefis::PropertyFloat	_ins_timestamp_s;
+	Xefis::PropertyAngle		_ins_longitude;
+	Xefis::PropertyAngle		_ins_latitude;
+	Xefis::PropertyLength		_ins_altitude_amsl;
+	Xefis::PropertyLength		_ins_accuracy;
+	Xefis::PropertyTime			_ins_timestamp;
 
 	// Output position:
-	Xefis::PropertyFloat	_position_longitude_deg;
-	Xefis::PropertyFloat	_position_latitude_deg;
-	Xefis::PropertyFloat	_position_altitude_amsl_ft;
-	Xefis::PropertyFloat	_position_accuracy_nm;
-	Xefis::PropertyString	_position_source;
+	Xefis::PropertyAngle		_position_longitude;
+	Xefis::PropertyAngle		_position_latitude;
+	Xefis::PropertyLength		_position_altitude_amsl;
+	Xefis::PropertyLength		_position_accuracy;
+	Xefis::PropertyString		_position_source;
 	// Output track (flight path):
-	Xefis::PropertyFloat	_track_vertical_deg;
-	Xefis::PropertyFloat	_track_true_heading_deg;
-	Xefis::PropertyFloat	_track_magnetic_heading_deg;
-	Xefis::PropertyFloat	_track_vertical_delta_dpf;
-	Xefis::PropertyFloat	_track_heading_delta_dpf;
+	Xefis::PropertyAngle		_track_vertical;
+	Xefis::PropertyAngle		_track_true_heading;
+	Xefis::PropertyAngle		_track_magnetic_heading;
+	Xefis::PropertyFloat		_track_vertical_delta_dpf;
+	Xefis::PropertyFloat		_track_heading_delta_dpf;
 	// Output orientation:
-	Xefis::PropertyFloat	_orientation_pitch_deg;
-	Xefis::PropertyFloat	_orientation_roll_deg;
-	Xefis::PropertyFloat	_orientation_true_heading_deg;
-	Xefis::PropertyFloat	_orientation_magnetic_heading_deg;
+	Xefis::PropertyAngle		_orientation_pitch;
+	Xefis::PropertyAngle		_orientation_roll;
+	Xefis::PropertyAngle		_orientation_true_heading;
+	Xefis::PropertyAngle		_orientation_magnetic_heading;
 	// Output altitude:
-	Xefis::PropertyFloat	_pressure_altitude_amsl_ft;
-	Xefis::PropertyFloat	_pressure_altitude_amsl_lookahead_ft;
-	Xefis::PropertyFloat	_pressure_altitude_amsl_time_s;
-	Xefis::PropertyFloat	_pressure_altitude_climb_rate_fpm;
+	Xefis::PropertyLength		_pressure_altitude_amsl;
+	Xefis::PropertyLength		_pressure_altitude_amsl_lookahead;
+	Xefis::PropertyTime			_pressure_altitude_amsl_time;
+	Xefis::PropertySpeed		_pressure_altitude_climb_rate;
 	// Output speeds:
-	Xefis::PropertyFloat	_v_r_kt;
-	Xefis::PropertyFloat	_v_ref_kt;
-	Xefis::PropertyFloat	_v_a_kt;
-	Xefis::PropertyFloat	_v_no_kt;
-	Xefis::PropertyFloat	_minimum_ias_kt;
-	Xefis::PropertyFloat	_minimum_maneuver_ias_kt;
-	Xefis::PropertyFloat	_maximum_ias_kt;
-	Xefis::PropertyFloat	_maximum_maneuver_ias_kt;
-	Xefis::PropertyFloat	_ias_lookahead_kt;
-	Xefis::PropertyFloat	_ias_lookahead_time_s;
-	Xefis::PropertyFloat	_true_airspeed_kt;
-	Xefis::PropertyFloat	_ground_speed_kt;
-	Xefis::PropertyFloat	_mach;
-	Xefis::PropertyFloat	_sound_speed_kt;
+	Xefis::PropertySpeed		_v_r;
+	Xefis::PropertySpeed		_v_ref;
+	Xefis::PropertySpeed		_v_a;
+	Xefis::PropertySpeed		_v_no;
+	Xefis::PropertySpeed		_minimum_ias;
+	Xefis::PropertySpeed		_minimum_maneuver_ias;
+	Xefis::PropertySpeed		_maximum_ias;
+	Xefis::PropertySpeed		_maximum_maneuver_ias;
+	Xefis::PropertySpeed		_ias_lookahead;
+	Xefis::PropertyTime			_ias_lookahead_time;
+	Xefis::PropertySpeed		_true_airspeed;
+	Xefis::PropertySpeed		_ground_speed;
+	Xefis::PropertyFloat		_mach;
+	Xefis::PropertySpeed		_sound_speed;
 	// Output AOA:
-	Xefis::PropertyFloat	_relative_pitch_limit_deg;
-	Xefis::PropertyFloat	_aoa_alpha_deg;
-	Xefis::PropertyFloat	_aoa_beta_deg;
+	Xefis::PropertyAngle		_relative_pitch_limit;
+	Xefis::PropertyAngle		_aoa_alpha;
+	Xefis::PropertyAngle		_aoa_beta;
 	// Output wind:
-	Xefis::PropertyFloat	_wind_true_orientation_from_deg;
-	Xefis::PropertyFloat	_wind_magnetic_orientation_from_deg;
-	Xefis::PropertyFloat	_wind_tas_kt;
+	Xefis::PropertyAngle		_wind_true_orientation_from;
+	Xefis::PropertyAngle		_wind_magnetic_orientation_from;
+	Xefis::PropertySpeed		_wind_tas;
 	// Output approach:
-	Xefis::PropertyFloat	_localizer_vertical_deviation_deg;
-	Xefis::PropertyFloat	_localizer_lateral_deviation_deg;
-	Xefis::PropertyFloat	_localizer_identifier;
-	Xefis::PropertyFloat	_localizer_source;
-	Xefis::PropertyFloat	_localizer_distance_nm;
-	Xefis::PropertyFloat	_localizer_frequency_hz;
+	Xefis::PropertyAngle		_localizer_vertical_deviation;
+	Xefis::PropertyAngle		_localizer_lateral_deviation;
+	Xefis::PropertyString		_localizer_identifier;
+	Xefis::PropertyString		_localizer_source;
+	Xefis::PropertyLength		_localizer_distance;
+	Xefis::PropertyFrequency	_localizer_frequency;
 	// Output other:
-	Xefis::PropertyFloat	_climb_glide_ratio;
-	Xefis::PropertyFloat	_magnetic_declination_deg;
-	Xefis::PropertyFloat	_magnetic_inclination_deg;
-	Xefis::PropertyFloat	_density_altitude_ft;
+	Xefis::PropertyFloat		_climb_glide_ratio;
+	Xefis::PropertyAngle		_magnetic_declination;
+	Xefis::PropertyAngle		_magnetic_inclination;
+	Xefis::PropertyLength		_density_altitude;
 };
 
 #endif
