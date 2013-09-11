7e86799efbb4700d0530a9b120f4b1333e8e945d
Michał Gawron <michal@gawron.name>
2013-09-11 22:04:32 +0200

    Add systems/slip-skid module.
    Computes slip-skid indicator based on acceleration vectors taken from IMU.

---
+++ src/modules/systems/slip_skid.cc
@@ -0,0 +1,73 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtXml/QDomElement>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/utility/qdom.h>
+
+// Local:
+#include "slip_skid.h"
+
+
+XEFIS_REGISTER_MODULE_CLASS ("systems/slip-skid", SlipSkid);
+
+
+SlipSkid::SlipSkid (Xefis::ModuleManager* module_manager, QDomElement const& config):
+	Module (module_manager, config)
+{
+	for (QDomElement& e: config)
+	{
+		if (e == "properties")
+		{
+			parse_properties (e, {
+				// Input:
+				{ "acceleration.x", _x_acceleration, true },
+				{ "acceleration.y", _y_acceleration, true },
+				{ "acceleration.z", _z_acceleration, true },
+				// Output:
+				{ "slip-skid", _slip_skid, true },
+			});
+		}
+	}
+
+	_slip_skid_computer.set_callback (std::bind (&SlipSkid::compute_slip_skid, this));
+	_slip_skid_computer.observe ({ &_x_acceleration, &_y_acceleration, &_z_acceleration });
+}
+
+
+void
+SlipSkid::data_updated()
+{
+	_slip_skid_computer.data_updated (update_time());
+}
+
+
+void
+SlipSkid::compute_slip_skid()
+{
+	if (_x_acceleration.valid() && _y_acceleration.valid() && _z_acceleration.valid())
+	{
+		float const scale = 10.f;
+		_slip_skid.write (scale * std::atan2 (*_y_acceleration, -*_z_acceleration) / (2.f * M_PI));
+		signal_data_updated();
+	}
+	else
+		_slip_skid.set_nil();
+}
+
---
+++ src/modules/systems/slip_skid.h
@@ -0,0 +1,56 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__SYSTEMS__SLIP_SKID_H__INCLUDED
+#define XEFIS__MODULES__SYSTEMS__SLIP_SKID_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtXml/QDomElement>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/module.h>
+#include <xefis/core/property.h>
+#include <xefis/core/property_observer.h>
+
+
+/**
+ * Computes slip-skid value from three-axis accelerometer values.
+ */
+class SlipSkid: public Xefis::Module
+{
+  public:
+	// Ctor
+	SlipSkid (Xefis::ModuleManager*, QDomElement const& config);
+
+  private:
+	void
+	data_updated() override;
+
+	void
+	compute_slip_skid();
+
+  private:
+	Xefis::PropertyObserver	_slip_skid_computer;
+	// Input:
+	Xefis::PropertyFloat	_x_acceleration;
+	Xefis::PropertyFloat	_y_acceleration;
+	Xefis::PropertyFloat	_z_acceleration;
+	// Output:
+	Xefis::PropertyFloat	_slip_skid;
+};
+
+#endif
