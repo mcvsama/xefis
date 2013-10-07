0f89886a591c3521d648b0e45aa388c28271abed
Michał Gawron <michal@gawron.name>
2013-10-07 21:31:00 +0200

    AirDataComputer (systems/adc) separated from FDC.

---
+++ src/modules/systems/adc.cc
@@ -0,0 +1,359 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtXml/QDomElement>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/config/exception.h>
+#include <xefis/utility/qdom.h>
+#include <xefis/airnav/density_altitude.h>
+#include <xefis/airnav/sound_speed.h>
+
+// Local:
+#include "adc.h"
+
+
+XEFIS_REGISTER_MODULE_CLASS ("systems/adc", AirDataComputer);
+
+
+AirDataComputer::AirDataComputer (Xefis::ModuleManager* module_manager, QDomElement const& config):
+	Module (module_manager, config)
+{
+	_altitude_amsl_estimator.set_minimum_integration_time (0.2_s);
+	_speed_ias_estimator.set_minimum_integration_time (0.2_s);
+
+	for (QDomElement& e: config)
+	{
+		if (e == "settings")
+		{
+			parse_settings (e, {
+				{ "ias.valid-minimum", _ias_valid_minimum, true },
+				{ "ias.valid-maximum", _ias_valid_maximum, true },
+			});
+		}
+		else if (e == "properties")
+		{
+			parse_properties (e, {
+				// Input:
+				{ "settings.pressure.use-std", _pressure_use_std, false },
+				{ "settings.pressure.qnh", _pressure_qnh, true },
+				{ "pressure.static.serviceable", _pressure_static_serviceable, false },
+				{ "pressure.static", _pressure_static, true },
+				{ "ias.serviceable", _ias_serviceable, false },
+				{ "ias", _ias, true },
+				{ "static-air-temperature", _static_air_temperature, true },
+				// Output:
+				{ "altitude.amsl.serviceable", _altitude_amsl_serviceable, true },
+				{ "altitude.amsl", _altitude_amsl, true },
+				{ "altitude.amsl.lookahead", _altitude_amsl_lookahead, true },
+				{ "altitude.amsl.qnh", _altitude_amsl_qnh, true },
+				{ "altitude.amsl.std", _altitude_amsl_std, true },
+				{ "density-altitude", _density_altitude, true },
+				{ "speed.ias.serviceable", _speed_ias_serviceable, true },
+				{ "speed.ias", _speed_ias, true },
+				{ "speed.ias.lookahead", _speed_ias_lookahead, true },
+				{ "speed.tas", _speed_tas, true },
+				{ "speed.mach", _speed_mach, true },
+				{ "speed.sound", _speed_sound, true },
+				{ "vertical-speed.serviceable", _vertical_speed_serviceable, true },
+				{ "vertical-speed", _vertical_speed, true },
+			});
+		}
+	}
+
+	_altitude_computer.set_callback (std::bind (&AirDataComputer::compute_altitude, this));
+	_altitude_computer.observe ({
+		&_pressure_static,
+		&_pressure_use_std,
+		&_pressure_qnh,
+		&_pressure_static_serviceable,
+	});
+
+	_density_altitude_computer.set_callback (std::bind (&AirDataComputer::compute_density_altitude, this));
+	_density_altitude_computer.observe ({
+		&_static_air_temperature,
+		&_altitude_amsl,
+	});
+
+	_ias_computer.set_callback (std::bind (&AirDataComputer::compute_ias, this));
+	_ias_computer.observe ({
+		&_ias,
+		&_ias_serviceable,
+	});
+
+	_ias_lookahead_computer.set_callback (std::bind (&AirDataComputer::compute_ias_lookahead, this));
+	_ias_lookahead_computer.observe ({
+		&_ias,
+	});
+
+	_sound_speed_computer.set_callback (std::bind (&AirDataComputer::compute_sound_speed, this));
+	_sound_speed_computer.observe ({
+		&_static_air_temperature,
+	});
+
+	_tas_computer.set_callback (std::bind (&AirDataComputer::compute_tas, this));
+	_tas_computer.observe ({
+		&_speed_ias,
+		&_density_altitude,
+		&_altitude_amsl,
+	});
+
+	_mach_computer.set_callback (std::bind (&AirDataComputer::compute_mach, this));
+	_mach_computer.observe ({
+		&_speed_tas,
+		&_speed_sound,
+	});
+
+	_vertical_speed_computer.set_callback (std::bind (&AirDataComputer::compute_vertical_speed, this));
+	_vertical_speed_computer.observe ({
+		&_altitude_amsl_std,
+	});
+}
+
+
+void
+AirDataComputer::data_updated()
+{
+	Xefis::PropertyObserver* computers[] = {
+		// Order is important:
+		&_altitude_computer,
+		&_density_altitude_computer,
+		&_ias_computer,
+		&_ias_lookahead_computer,
+		&_sound_speed_computer,
+		&_tas_computer,
+		&_mach_computer,
+		&_vertical_speed_computer,
+	};
+
+	for (Xefis::PropertyObserver* o: computers)
+		o->data_updated (update_time());
+}
+
+
+void
+AirDataComputer::compute_altitude()
+{
+	Time update_time = _altitude_computer.update_time();
+	Time update_dt = _altitude_computer.update_dt();
+
+	if (_pressure_static.valid() &&
+		((_pressure_use_std.read (false)) || _pressure_qnh.valid()))
+	{
+		auto compute_altitude = [&](Pressure pressure_setting) -> Length {
+			// Good for heights below tropopause (36 kft):
+			double a = 6.8755856e-6;
+			double b = 5.2558797;
+			double p = (*_pressure_static).inHg();
+			double p0 = pressure_setting.inHg();
+			return 1_ft * -(std::pow (p / p0, 1.0 / b) - 1.0) / a;
+		};
+
+		bool hide_alt_lookahead = false;
+		Pressure pressure_setting;
+		if (_pressure_use_std.read (false))
+		{
+			pressure_setting = 29.92_inHg;
+			if (!_prev_use_standard_pressure)
+				hide_alt_lookahead = true;
+			_prev_use_standard_pressure = true;
+		}
+		else
+		{
+			pressure_setting = *_pressure_qnh;
+			if (_prev_use_standard_pressure)
+				hide_alt_lookahead = true;
+			_prev_use_standard_pressure = false;
+		}
+
+		if (hide_alt_lookahead)
+			_hide_alt_lookahead_until = update_time + _altitude_amsl_lookahead_o_smoother.smoothing_time() + _altitude_amsl_lookahead_i_smoother.smoothing_time();
+
+		Length height = compute_altitude (pressure_setting);
+		Length qnh_height = compute_altitude (*_pressure_qnh);
+		Length std_height = compute_altitude (29.92_inHg);
+
+		_altitude_amsl.write (1_ft * _altitude_amsl_smoother.process (height.ft(), update_dt));
+		_altitude_amsl_qnh.write (1_ft * _altitude_amsl_qnh_smoother.process (qnh_height.ft(), update_dt));
+		_altitude_amsl_std.write (1_ft * _altitude_amsl_std_smoother.process (std_height.ft(), update_dt));
+	}
+	else
+	{
+		_altitude_amsl.set_nil();
+		_altitude_amsl_qnh.set_nil();
+		_altitude_amsl_std.set_nil();
+		_altitude_amsl_smoother.invalidate();
+		_altitude_amsl_qnh_smoother.invalidate();
+		_altitude_amsl_std_smoother.invalidate();
+	}
+
+	if (_altitude_amsl.valid() && update_time > _hide_alt_lookahead_until)
+	{
+		double est = _altitude_amsl_estimator.process (_altitude_amsl_lookahead_i_smoother.process ((*_altitude_amsl).ft(), update_dt), update_dt);
+		est = _altitude_amsl_lookahead_o_smoother.process (est, update_dt);
+		_altitude_amsl_lookahead.write (1_ft * est);
+	}
+	else
+	{
+		_altitude_amsl_lookahead.set_nil();
+		_altitude_amsl_estimator.invalidate();
+		_altitude_amsl_lookahead_i_smoother.invalidate();
+		_altitude_amsl_lookahead_o_smoother.invalidate();
+	}
+
+	_altitude_amsl_serviceable.copy (_pressure_static_serviceable);
+}
+
+
+void
+AirDataComputer::compute_density_altitude()
+{
+	if (_static_air_temperature.valid() && _altitude_amsl.valid())
+	{
+		Xefis::DensityAltitude da;
+		da.set_pressure_altitude (*_altitude_amsl);
+		da.set_static_air_temperature (*_static_air_temperature);
+		da.update();
+		_density_altitude.write (da.density_altitude());
+	}
+	else
+		_density_altitude.set_nil();
+}
+
+
+void
+AirDataComputer::compute_ias()
+{
+	Time update_dt = _ias_computer.update_dt();
+
+	if (_ias.valid())
+		_speed_ias.write (1_kt * _speed_ias_smoother.process ((*_ias).kt(), update_dt));
+	else
+	{
+		_speed_ias.set_nil();
+		_speed_ias_smoother.invalidate();
+	}
+
+	_ias_in_valid_range = _speed_ias.valid() && _ias_valid_minimum <= *_speed_ias && *_speed_ias <= _ias_valid_maximum;
+
+	_speed_ias_serviceable.copy (_ias_serviceable);
+}
+
+
+void
+AirDataComputer::compute_ias_lookahead()
+{
+	if (_ias_in_valid_range)
+	{
+		Time update_dt = _ias_lookahead_computer.update_dt();
+
+		double est = _speed_ias_estimator.process (_speed_ias_lookahead_i_smoother.process ((*_ias).kt(), update_dt), update_dt);
+		est = _speed_ias_lookahead_o_smoother.process (est, update_dt);
+		_speed_ias_lookahead.write (1_kt * est);
+	}
+	else
+	{
+		_speed_ias_lookahead.set_nil();
+		_speed_ias_estimator.invalidate();
+		_speed_ias_lookahead_i_smoother.invalidate();
+		_speed_ias_lookahead_o_smoother.invalidate();
+	}
+}
+
+
+void
+AirDataComputer::compute_sound_speed()
+{
+	if (_static_air_temperature.valid())
+	{
+		Xefis::SoundSpeed ss;
+		ss.set_static_air_temperature (*_static_air_temperature);
+		ss.update();
+		_speed_sound.write (ss.sound_speed());
+	}
+	else
+		_speed_sound.set_nil();
+}
+
+
+void
+AirDataComputer::compute_tas()
+{
+	if (_ias_in_valid_range && _altitude_amsl.valid())
+	{
+		Speed cas = *_speed_ias;
+
+		if (_density_altitude.valid())
+		{
+			// This does not take into account air compressibility factor, so it's valid
+			// for low speeds (mach < 0.3) and altitude below tropopause (36 kft):
+			_speed_tas.write (cas / std::pow (1 - 6.8755856 * 1e-6 * (*_density_altitude).ft(), 2.127940));
+		}
+		else
+			// Very simple equation for TAS, fix it to use air temperature someday:
+			_speed_tas.write (cas + 0.02 * cas * (*_altitude_amsl / 1000_ft));
+	}
+	else
+		_speed_tas.set_nil();
+}
+
+
+void
+AirDataComputer::compute_mach()
+{
+	// The approximate speed of sound in dry (0% humidity) air:
+	if (_speed_tas.valid() && _speed_sound.valid())
+		_speed_mach.write (*_speed_tas / *_speed_sound);
+	else
+		_speed_mach.set_nil();
+}
+
+
+void
+AirDataComputer::compute_vertical_speed()
+{
+	// Vertical speed:
+	if (_altitude_amsl_std.valid())
+	{
+		Time update_dt = _vertical_speed_computer.update_dt();
+
+		// If previous climb-rate was invalid, use current STD pressure
+		// as source for 'previous altitude' value:
+		if (_vertical_speed.is_nil())
+			_prev_altitude_amsl = *_altitude_amsl_std;
+
+		Time& time = _vertical_speed_time;
+		time += update_dt;
+		if (time > 0.05_s)
+		{
+			Length alt_diff = *_altitude_amsl_std - _prev_altitude_amsl;
+			Speed computed_vertical_speed = alt_diff / time;
+			time = 0_s;
+			_prev_altitude_amsl = *_altitude_amsl_std;
+			_vertical_speed.write (1_fpm * _vertical_speed_smoother.process (computed_vertical_speed.fpm(), update_dt));
+		}
+	}
+	else
+	{
+		_vertical_speed.set_nil();
+		_vertical_speed_smoother.invalidate();
+	}
+
+	_vertical_speed_serviceable.write (_vertical_speed.valid());
+}
+
---
+++ src/modules/systems/adc.h
@@ -0,0 +1,125 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__SYSTEMS__ADC_H__INCLUDED
+#define XEFIS__MODULES__SYSTEMS__ADC_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <array>
+
+// Qt:
+#include <QtXml/QDomElement>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/module.h>
+#include <xefis/core/property.h>
+#include <xefis/core/property_observer.h>
+#include <xefis/utility/smoother.h>
+#include <xefis/utility/lookahead.h>
+
+
+/**
+ * Computations are reliable up to 36,000 ft altitude
+ * and about 0.3 mach speed.
+ */
+class AirDataComputer: public Xefis::Module
+{
+  public:
+	// Ctor
+	AirDataComputer (Xefis::ModuleManager*, QDomElement const& config);
+
+  private:
+	void
+	data_updated() override;
+
+	void
+	compute_altitude();
+
+	void
+	compute_density_altitude();
+
+	void
+	compute_ias();
+
+	void
+	compute_ias_lookahead();
+
+	void
+	compute_sound_speed();
+
+	void
+	compute_tas();
+
+	void
+	compute_mach();
+
+	void
+	compute_vertical_speed();
+
+  private:
+	bool						_ias_in_valid_range					= false;
+	bool						_prev_use_standard_pressure			= false;
+	Time						_hide_alt_lookahead_until			= 0_s;
+	Length						_prev_altitude_amsl					= 0_ft;
+	Time						_vertical_speed_time				= 0_s;
+	Xefis::Smoother<double>		_vertical_speed_smoother			= 1_s;
+	Xefis::Smoother<double>		_altitude_amsl_smoother				= 500_ms;
+	Xefis::Smoother<double>		_altitude_amsl_qnh_smoother			= 500_ms;
+	Xefis::Smoother<double>		_altitude_amsl_std_smoother			= 500_ms;
+	Xefis::Smoother<double>		_speed_ias_smoother					= 100_ms;
+	Xefis::Smoother<double>		_altitude_amsl_lookahead_i_smoother	= 100_ms;
+	Xefis::Smoother<double>		_altitude_amsl_lookahead_o_smoother	= 500_ms;
+	Xefis::Smoother<double>		_speed_ias_lookahead_i_smoother		= 100_ms;
+	Xefis::Smoother<double>		_speed_ias_lookahead_o_smoother		= 1000_ms;
+	Xefis::Lookahead<double>	_altitude_amsl_estimator			= Xefis::Lookahead<double> (10_s);
+	Xefis::Lookahead<double>	_speed_ias_estimator				= Xefis::Lookahead<double> (10_s);
+	// Property observers:
+	Xefis::PropertyObserver		_altitude_computer;
+	Xefis::PropertyObserver		_density_altitude_computer;
+	Xefis::PropertyObserver		_ias_computer;
+	Xefis::PropertyObserver		_ias_lookahead_computer;
+	Xefis::PropertyObserver		_sound_speed_computer;
+	Xefis::PropertyObserver		_tas_computer;
+	Xefis::PropertyObserver		_mach_computer;
+	Xefis::PropertyObserver		_vertical_speed_computer;
+	// Settings:
+	Speed						_ias_valid_minimum;
+	Speed						_ias_valid_maximum;
+	// Input:
+	Xefis::PropertyBoolean		_pressure_use_std;
+	Xefis::PropertyPressure		_pressure_qnh;
+	Xefis::PropertyBoolean		_pressure_static_serviceable;
+	Xefis::PropertyPressure		_pressure_static;
+	Xefis::PropertyBoolean		_ias_serviceable;
+	Xefis::PropertySpeed		_ias;
+	Xefis::PropertyTemperature	_static_air_temperature;
+	// Output:
+	Xefis::PropertyBoolean		_altitude_amsl_serviceable;
+	Xefis::PropertyLength		_altitude_amsl;
+	Xefis::PropertyLength		_altitude_amsl_lookahead;
+	Xefis::PropertyLength		_altitude_amsl_qnh;
+	Xefis::PropertyLength		_altitude_amsl_std;
+	Xefis::PropertyLength		_density_altitude;
+	Xefis::PropertyBoolean		_speed_ias_serviceable;
+	Xefis::PropertySpeed		_speed_ias;
+	Xefis::PropertySpeed		_speed_ias_lookahead;
+	Xefis::PropertySpeed		_speed_tas;
+	Xefis::PropertyFloat		_speed_mach;
+	Xefis::PropertySpeed		_speed_sound;
+	Xefis::PropertyBoolean		_vertical_speed_serviceable;
+	Xefis::PropertySpeed		_vertical_speed;
+};
+
+#endif
