503fe50f0d3c4981f6e4a55f9c26f58d27ffe583
Michał Gawron <michal@gawron.name>
2013-10-12 01:27:24 +0200

    Use "settings" for I2C configuration rather than parse_i2c().

--- src/modules/io/ht16k33.cc
+++ src/modules/io/ht16k33.cc
@@ -269,9 +269,19 @@ HT16K33::SingleSwitch::invalidate()
 HT16K33::HT16K33 (Xefis::ModuleManager* module_manager, QDomElement const& config):
 	Module (module_manager, config)
 {
+	Xefis::I2C::Bus::ID i2c_bus;
+	Xefis::I2C::Address::ID i2c_address;
+
 	for (QDomElement& e: config)
 	{
-		if (e == "properties")
+		if (e == "settings")
+		{
+			parse_settings (e, {
+				{ "i2c.bus", i2c_bus, true },
+				{ "i2c.address", i2c_address, true },
+			});
+		}
+		else if (e == "properties")
 		{
 			parse_properties (e, {
 				{ "enabled", _enabled, false },
@@ -280,8 +290,6 @@ HT16K33::HT16K33 (Xefis::ModuleManager* module_manager, QDomElement const& confi
 				{ "blinking.mode", _blinking_mode, false },
 			});
 		}
-		else if (e == "i2c")
-			parse_i2c (e, _i2c_device);
 		else if (e == "input")
 		{
 			_reliable_mode = e.attribute ("reliable-mode") == "true";
@@ -317,6 +325,9 @@ HT16K33::HT16K33 (Xefis::ModuleManager* module_manager, QDomElement const& confi
 			}
 		}
 	}
+
+	_i2c_device.bus().set_bus_number (i2c_bus);
+	_i2c_device.set_address (Xefis::I2C::Address (i2c_address));
 
 	_reinitialize_timer = new QTimer (this);
 	_reinitialize_timer->setInterval (250);
