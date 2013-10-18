64b7fa7b0b555c6d067632f058017bcd5898a148
Michał Gawron <michal@gawron.name>
2013-10-18 20:27:24 +0200

    Add LinearIndicator instrument.

---
+++ src/modules/instruments/linear_indicator.cc
@@ -0,0 +1,86 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtWidgets/QLayout>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/services.h>
+#include <xefis/utility/numeric.h>
+#include <xefis/utility/painter.h>
+#include <xefis/utility/qdom.h>
+
+// Local:
+#include "linear_indicator.h"
+
+
+XEFIS_REGISTER_MODULE_CLASS ("instruments/linear-indicator", LinearIndicator);
+
+
+LinearIndicator::LinearIndicator (Xefis::ModuleManager* module_manager, QDomElement const& config):
+	Xefis::Instrument (module_manager, config)
+{
+	_widget = new LinearIndicatorWidget (this);
+
+	QVBoxLayout* layout = new QVBoxLayout (this);
+	layout->setMargin (0);
+	layout->setSpacing (0);
+	layout->addWidget (_widget);
+
+	for (QDomElement& e: config)
+	{
+		if (e == "settings")
+		{
+			parse_settings (e, {
+				{ "style.mirrored", _style_mirrored, false },
+				{ "value.precision", _value_precision, false },
+				{ "value.modulo", _value_modulo, false },
+				{ "value.digits", _value_digits, false },
+				{ "value.minimum", _value_minimum, true },
+				{ "value.minimum.critical", _value_minimum_critical, false },
+				{ "value.minimum.warning", _value_minimum_warning, false },
+				{ "value.maximum.warning", _value_maximum_warning, false },
+				{ "value.maximum.critical", _value_maximum_critical, false },
+				{ "value.maximum", _value_maximum, true },
+			});
+		}
+		else if (e == "properties")
+		{
+			parse_properties (e, {
+				{ "value", _value, true },
+			});
+		}
+	}
+}
+
+
+void
+LinearIndicator::data_updated()
+{
+	_widget->set_mirrored_style (_style_mirrored);
+	_widget->set_range (Xefis::Range<double> { _value_minimum, _value_maximum });
+	_widget->set_precision (_value_precision);
+	_widget->set_modulo (_value_modulo);
+	_widget->set_digits (_value_digits);
+
+	_widget->set_value (_value.get_optional());
+	_widget->set_minimum_critical_value (_value_minimum_critical);
+	_widget->set_minimum_warning_value (_value_minimum_warning);
+	_widget->set_maximum_warning_value (_value_maximum_warning);
+	_widget->set_maximum_critical_value (_value_maximum_critical);
+}
+
---
+++ src/modules/instruments/linear_indicator_widget.cc
@@ -0,0 +1,159 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+#include <vector>
+
+// Qt:
+#include <QtGui/QPainter>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/window.h>
+#include <xefis/utility/numeric.h>
+
+// Local:
+#include "linear_indicator_widget.h"
+
+
+LinearIndicatorWidget::LinearIndicatorWidget (QWidget* parent):
+	InstrumentWidget (parent),
+	InstrumentAids (0.8f)
+{ }
+
+
+void
+LinearIndicatorWidget::resizeEvent (QResizeEvent* event)
+{
+	InstrumentWidget::resizeEvent (event);
+
+	auto xw = dynamic_cast<Xefis::Window*> (window());
+	if (xw)
+		InstrumentAids::set_scaling (1.2f * xw->pen_scale(), 0.95f * xw->font_scale());
+
+	InstrumentAids::update_sizes (size(), window()->size());
+}
+
+
+void
+LinearIndicatorWidget::paintEvent (QPaintEvent*)
+{
+	float const w = width();
+	float const h = height();
+
+	Xefis::Painter painter (this, &_text_painter_cache);
+	painter.setRenderHint (QPainter::Antialiasing, true);
+	painter.setRenderHint (QPainter::TextAntialiasing, true);
+	painter.setRenderHint (QPainter::SmoothPixmapTransform, true);
+	painter.setRenderHint (QPainter::NonCosmeticDefaultPen, true);
+
+	QPen pen_white = get_pen (Qt::white, 1.f);
+	QPen pen_silver = get_pen (QColor (0xbb, 0xbd, 0xbf), 1.f);
+
+	if (_mirrored)
+	{
+		painter.translate (w, 0.f);
+		painter.scale (-1.f, 1.f);
+	}
+
+	// Clear with black background:
+	painter.setPen (Qt::NoPen);
+	painter.setBrush (Qt::black);
+	painter.drawRect (rect());
+
+	float q = 0.06f * wh();
+	float m = 0.7f * q;
+	QRectF area (m, m, w - 2.f * m, h - 2.f * m);
+
+	QPointF p0 (area.right() - 3.f * q, area.top());
+	QPointF p1 (area.right() - 3.f * q, area.bottom());
+
+	// Indicator
+
+	painter.setPen (pen_silver);
+	painter.drawLine (p0, p1);
+
+	if (_value)
+	{
+		float value = Xefis::limit<float> (*_value, _range.min(), _range.max());
+		bool inbound = _range.includes (*_value);
+
+		if (inbound)
+			painter.setBrush (Qt::white);
+		else
+			painter.setBrush (Qt::NoBrush);
+
+		painter.setPen (pen_white);
+		QPolygonF polygon = QPolygonF()
+			<< QPointF (0.f, 0.f)
+			<< QPointF (1.9f * q, -0.5f * q)
+			<< QPointF (1.9f * q, +0.5f * q);
+		polygon.translate (p1.x(), Xefis::renormalize (value, _range.min(), _range.max(), p1.y(), p0.y()));
+		painter.add_shadow ([&]() {
+			painter.drawPolygon (polygon);
+		});
+	}
+
+	// Text
+
+	QFont font (_font_20);
+	QFontMetricsF metrics (font);
+	float char_width = metrics.width ("0");
+	float ycorr = 0.04f * metrics.height();
+	float hcorr = 2.f * ycorr;
+
+	QString text;
+	if (_value)
+		text = stringify_value (*_value);
+	text = pad_string (text);
+
+	painter.setFont (font);
+	QRectF text_rect = painter.get_text_box (QPointF (p0.x() - q, h / 2.f), Qt::AlignRight | Qt::AlignVCenter, text);
+	text_rect.adjust (-0.5f * char_width, 0, 0.f, -2.f * hcorr);
+	painter.setPen (pen_white);
+	painter.setBrush (Qt::NoBrush);
+	painter.drawRect (text_rect);
+	QPointF position;
+	if (_mirrored)
+	{
+   		position = QPointF (text_rect.left() + 0.25f * char_width, text_rect.center().y() + ycorr);
+		position = painter.transform().map (position);
+		painter.resetTransform();
+	}
+	else
+	{
+   		position = QPointF (text_rect.right() - 0.25f * char_width, text_rect.center().y() + ycorr);
+	}
+	painter.fast_draw_text (position, Qt::AlignVCenter | Qt::AlignRight, text);
+}
+
+
+QString
+LinearIndicatorWidget::stringify_value (double value) const
+{
+	double numeric_value = value;
+	if (_precision < 0)
+		numeric_value /= std::pow (10.0, -_precision);
+	if (_modulo > 0)
+		numeric_value = static_cast<int> (numeric_value) / _modulo * _modulo;
+	return QString ("%1").arg (numeric_value, 0, 'f', std::max (0, _precision));
+}
+
+
+QString
+LinearIndicatorWidget::pad_string (QString const& input) const
+{
+	return QString ("%1").arg (input, _digits);
+}
+
