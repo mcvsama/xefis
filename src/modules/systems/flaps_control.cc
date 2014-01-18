d10e1b16a0987345a241c32f07c3681677b18ed4
Michał Gawron <michal@gawron.name>
2014-01-18 12:04:31 +0100

    Flaps control module + configuration.

---
+++ src/modules/systems/flaps_control.cc
@@ -0,0 +1,125 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/utility/qdom.h>
+#include <xefis/utility/numeric.h>
+
+// Local:
+#include "flaps_control.h"
+
+
+XEFIS_REGISTER_MODULE_CLASS ("systems/flaps-control", FlapsControl);
+
+
+constexpr Time FlapsControl::kUpdateInterval;
+
+
+FlapsControl::FlapsControl (Xefis::ModuleManager* module_manager, QDomElement const& config):
+	Module (module_manager, config)
+{
+	QString settings_list_str;
+
+	parse_settings (config, {
+		{ "settings-list", settings_list_str, true },
+		{ "degrees-per-second", _degrees_per_second, false },
+		{ "control.minimum", _ctl_minimum, false },
+		{ "control.maximum", _ctl_maximum, false },
+	});
+
+	parse_properties (config, {
+		{ "input.up", _input_up, false },
+		{ "input.down", _input_down, false },
+		{ "input.setting", _input_setting, true },
+		{ "output.setting", _output_setting, false },
+		{ "output.current", _output_current, false },
+		{ "output.control", _output_control, false },
+	});
+
+	for (auto s: settings_list_str.split (","))
+		_settings_list.insert (1_deg * s.toDouble());
+
+	if (_settings_list.empty())
+		throw Xefis::Exception ("settings list must not be empty");
+
+	_timer = std::make_unique<QTimer>();
+	_timer->setInterval (kUpdateInterval.ms());
+	_timer->setSingleShot (false);
+	QObject::connect (_timer.get(), &QTimer::timeout, std::bind (&FlapsControl::update_flap_position, this));
+
+	_minimum = *_settings_list.begin();
+	_maximum = *_settings_list.rbegin();
+	_current = _minimum;
+}
+
+
+void
+FlapsControl::data_updated()
+{
+	if (!_input_setting.valid())
+		_input_setting.write (0_deg);
+
+	if (_input_up.fresh() && _input_up.read (false))
+	{
+		auto prev_setting = _settings_list.lower_bound (_input_setting.read (0_deg));
+		if (prev_setting != _settings_list.begin())
+			prev_setting--;
+		_input_setting.write (*prev_setting);
+	}
+	else if (_input_down.fresh() && _input_down.read (false))
+	{
+		auto next_setting = _settings_list.upper_bound (_input_setting.read (0_deg));
+		if (next_setting != _settings_list.end())
+			_input_setting.write (*next_setting);
+	}
+
+	if (_input_setting.fresh() && _input_setting.valid())
+	{
+		_setting = Xefis::limit (*_input_setting, _minimum, _maximum);
+		if (_output_setting.configured())
+			_output_setting.write (_setting);
+		_timer->start();
+	}
+}
+
+
+void
+FlapsControl::update_flap_position()
+{
+	double sgn = Xefis::sgn ((_setting - _current).deg());
+	Angle difference = _setting - _current;
+	Angle delta = 1_deg * (kUpdateInterval.s() * _degrees_per_second);
+
+	if (std::abs (difference.deg()) > delta.deg())
+	{
+		Angle diff_to_add = sgn * delta;
+		if (std::abs (diff_to_add.deg()) > std::abs (difference.deg()))
+			diff_to_add = difference;
+		_current += sgn * delta;
+	}
+	else
+	{
+		_current = _setting;
+		_timer->stop();
+	}
+
+	if (_output_current.configured())
+		_output_current.write (_current);
+	if (_output_control.configured())
+		_output_control.write (Xefis::renormalize (_current.deg(), _minimum.deg(), _maximum.deg(), _ctl_minimum, _ctl_maximum));
+}
+
---
+++ src/modules/systems/flaps_control.h
@@ -0,0 +1,68 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__SYSTEMS__FLAPS_CONTROL_H__INCLUDED
+#define XEFIS__MODULES__SYSTEMS__FLAPS_CONTROL_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <set>
+
+// Qt:
+#include <QtCore/QTimer>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/module.h>
+#include <xefis/core/property.h>
+
+
+class FlapsControl: public Xefis::Module
+{
+	static constexpr Time kUpdateInterval = 10_ms;
+
+  public:
+	// Ctor
+	FlapsControl (Xefis::ModuleManager*, QDomElement const& config);
+
+  protected:
+	// Module API
+	void
+	data_updated() override;
+
+  private:
+	void
+	update_flap_position();
+
+  private:
+	std::set<Angle>			_settings_list;
+	double					_ctl_minimum		= 0.0;
+	double					_ctl_maximum		= 1.0;
+	Angle					_minimum;
+	Angle					_maximum;
+	Angle					_setting;
+	Angle					_current;
+	double					_degrees_per_second	= 100.0;
+	// Input:
+	Xefis::PropertyBoolean	_input_up;
+	Xefis::PropertyBoolean	_input_down;
+	Xefis::PropertyAngle	_input_setting;
+	// Output:
+	Xefis::PropertyAngle	_output_setting;
+	Xefis::PropertyAngle	_output_current;
+	Xefis::PropertyFloat	_output_control;
+	// Other:
+	Unique<QTimer>			_timer;
+};
+
+#endif
