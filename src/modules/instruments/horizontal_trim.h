f8d9fe8769a74aad00219a4def6bef2c9fe0aaff
Michał Gawron <michal@gawron.name>
2014-01-21 23:13:11 +0100

    Add HorizontalTrim instrument.

---
+++ src/modules/instruments/horizontal_trim.h
@@ -0,0 +1,65 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__INSTRUMENTS__HORIZONTAL_TRIM_H__INCLUDED
+#define XEFIS__MODULES__INSTRUMENTS__HORIZONTAL_TRIM_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtWidgets/QWidget>
+#include <QtXml/QDomElement>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/instrument.h>
+#include <xefis/core/instrument_aids.h>
+#include <xefis/core/property.h>
+
+
+class HorizontalTrim:
+	public Xefis::Instrument,
+	public Xefis::InstrumentAids
+{
+  public:
+	// Ctor
+	HorizontalTrim (Xefis::ModuleManager*, QDomElement const& config);
+
+	void
+	data_updated() override;
+
+  protected:
+	void
+	resizeEvent (QResizeEvent*) override;
+
+	void
+	paintEvent (QPaintEvent*) override;
+
+  private:
+	static QString
+	stringify (double value);
+
+  private:
+	// Settings:
+	QString					_label = "TRIM";
+	QString					_label_min;
+	QString					_label_max;
+	// Properties:
+	Xefis::PropertyFloat	_input_trim_value;
+	Xefis::PropertyFloat	_input_trim_reference;
+	Xefis::PropertyFloat	_input_trim_reference_minimum;
+	Xefis::PropertyFloat	_input_trim_reference_maximum;
+};
+
+#endif
