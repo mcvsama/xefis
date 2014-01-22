e71f7a1c65272bc46b6997ad13ca6d272574e1a0
Michał Gawron <michal@gawron.name>
2014-01-22 23:45:06 +0100

    Details.

--- src/modules/systems/trim_control.cc
+++ src/modules/systems/trim_control.cc
@@ -112,7 +112,7 @@ void
 TrimControl::update_trim()
 {
 	update_trim_without_sound();
-	this->module_manager()->application()->sound_manager()->play (XEFIS_SHARED_DIRECTORY "/sounds/trim-bip.wav");
+	module_manager()->application()->sound_manager()->play (XEFIS_SHARED_DIRECTORY "/sounds/trim-bip.wav");
 }
 
 
