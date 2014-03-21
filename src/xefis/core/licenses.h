25aabb22f9921f5ea6c67cf50b8cffc44384bc9a
Michał Gawron <michal@gawron.name>
2014-03-21 18:37:54 +0100

    Add --copyright option.

---
+++ src/xefis/core/licenses.h
@@ -0,0 +1,36 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2014  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__APPLICATION__LICENSES_H__INCLUDED
+#define XEFIS__APPLICATION__LICENSES_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+// Xefis:
+#include <xefis/config/all.h>
+
+
+namespace Xefis {
+namespace License {
+
+extern const char* main;
+extern const char* font_crystal;
+extern const char* lib_half;
+extern const char* lib_kdtreeplusplus;
+
+} // namespace License
+} // namespace Xefis
+
+#endif
+
