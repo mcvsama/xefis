bd12c41c3b2ecaa16a1ffbb82142d2936a9526a1
Michał Gawron <michal@gawron.name>
2016-01-01 15:27:47 +0100

    Run tests from separate binary "selftest"

---
+++ src/xefis/selftest.cc
@@ -0,0 +1,40 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standards:
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+// System:
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <locale.h>
+#include <string.h>
+#include <errno.h>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/fail.h>
+
+
+int
+main (int, char**, char**)
+{
+	return EXIT_SUCCESS;
+}
+
