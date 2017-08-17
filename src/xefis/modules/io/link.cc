977ed81cd3c048587b88369eafe349bbdb5a966f
Michał Gawron <michal@gawron.name>
2017-08-17 12:23:30 +0200

    Holy shit! I managed to refactor the io/link module!
    Not only that, I also added a test. A TEST!!!

---
+++ src/xefis/modules/io/link.cc
@@ -0,0 +1,540 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+#include <memory>
+#include <random>
+
+// Qt:
+#include <QtXml/QDomElement>
+
+// Lib:
+#include <boost/endian/conversion.hpp>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/stdexcept.h>
+#include <xefis/utility/qdom.h>
+#include <xefis/utility/qdom_iterator.h>
+#include <xefis/utility/hextable.h>
+#include <xefis/utility/hmac.h>
+#include <xefis/utility/blob.h>
+
+// Local:
+#include "link.h"
+
+
+LinkProtocol::Sequence::Sequence (PacketList packets):
+	_packets (packets)
+{ }
+
+
+Blob::size_type
+LinkProtocol::Sequence::size() const
+{
+	Blob::size_type s = 0;
+
+	for (auto const& packet: _packets)
+		s += packet->size();
+
+	return s;
+}
+
+
+void
+LinkProtocol::Sequence::produce (Blob& blob)
+{
+	for (auto const& packet: _packets)
+		packet->produce (blob);
+}
+
+
+Blob::const_iterator
+LinkProtocol::Sequence::eat (Blob::const_iterator begin, Blob::const_iterator end)
+{
+	for (auto const& packet: _packets)
+		begin = packet->eat (begin, end);
+
+	return begin;
+}
+
+
+void
+LinkProtocol::Sequence::apply()
+{
+	for (auto const& packet: _packets)
+		packet->apply();
+}
+
+
+void
+LinkProtocol::Sequence::failsafe()
+{
+	for (auto const& packet: _packets)
+		packet->failsafe();
+}
+
+
+LinkProtocol::Bitfield::Bitfield (std::initializer_list<SourceVariant> sources):
+	_bit_sources (sources)
+{
+	Blob::size_type total_bits = 0;
+
+	for (auto const& bsvariant: _bit_sources)
+	{
+		std::visit ([&total_bits](auto&& bs) noexcept {
+			total_bits += bs.bits;
+		}, bsvariant);
+	}
+
+	_size = (total_bits + 7) / 8;
+}
+
+
+Blob::size_type
+LinkProtocol::Bitfield::size() const
+{
+	return _size;
+}
+
+
+void
+LinkProtocol::Bitfield::produce (Blob& blob)
+{
+	std::vector<bool> bits;
+	bits.reserve (8 * size());
+
+	for (auto const& bsvariant: _bit_sources)
+	{
+		std::visit ([&bits](auto&& bs) {
+			uint_least64_t v = bs.fallback_value;
+
+			if (bs.property && fits_in_bits (*bs.property, Bits (bs.bits)))
+				v = *bs.property;
+
+			for (uint8_t b = 0; b < bs.bits; ++b)
+				bits.push_back ((v >> b) & 1);
+		}, bsvariant);
+	}
+
+	bits.resize (8 * size(), 0);
+
+	for (std::vector<bool>::size_type b = 0; b < bits.size(); b += 8)
+	{
+		uint8_t byte = 0;
+
+		for (std::vector<bool>::size_type k = 0; k < 8; ++k)
+			if (bits[k + b])
+				byte |= 1 << k;
+
+		blob.push_back (byte);
+	}
+}
+
+
+Blob::const_iterator
+LinkProtocol::Bitfield::eat (Blob::const_iterator begin, Blob::const_iterator end)
+{
+	if (std::distance (begin, end) < static_cast<Blob::difference_type> (size()))
+		throw InsufficientDataError();
+
+	std::vector<bool> bits;
+	bits.reserve (8 * size());
+
+	for (Blob::const_iterator cur = begin; cur < begin + size(); ++cur)
+		for (uint8_t b = 0; b < 8; ++b)
+			bits.push_back ((*cur >> b) & 1);
+
+	std::vector<bool>::iterator bit = bits.begin();
+
+	for (auto& bsvariant: _bit_sources)
+	{
+		std::visit ([&bit, &bits](auto&& bs) {
+			uint_least64_t v = 0;
+
+			for (uint8_t b = 0; b < bs.bits; ++b)
+				if (*(bit + b))
+					v |= 1 << b;
+
+			bs.value = v;
+			std::advance (bit, bs.bits);
+		}, bsvariant);
+	}
+
+	return begin + size();
+}
+
+
+void
+LinkProtocol::Bitfield::apply()
+{
+	for (auto& bsvariant: _bit_sources)
+	{
+		std::visit ([](auto&& bs) {
+			bs.property = bs.value;
+		}, bsvariant);
+	}
+}
+
+
+void
+LinkProtocol::Bitfield::failsafe()
+{
+	for (auto& bsvariant: _bit_sources)
+	{
+		std::visit ([](auto&& bs) {
+			if (!bs.retained)
+				bs.property.set_nil();
+		}, bsvariant);
+	}
+}
+
+
+LinkProtocol::Signature::Signature (NonceBytes nonce_bytes, SignatureBytes signature_bytes, Key key, PacketList packets):
+	Sequence (packets),
+	_nonce_bytes (*nonce_bytes),
+	_signature_bytes (*signature_bytes),
+	_key (*key),
+	_random_device(),
+	_rng (_random_device())
+{
+	_temp.reserve (size());
+}
+
+
+Blob::size_type
+LinkProtocol::Signature::size() const
+{
+	return Sequence::size() + _nonce_bytes + _signature_bytes;
+}
+
+
+void
+LinkProtocol::Signature::produce (Blob& blob)
+{
+	_temp.clear();
+
+	// Add data:
+	Sequence::produce (_temp);
+
+	// Append nonce:
+	std::uniform_int_distribution<uint8_t> dist;
+
+	for (unsigned int i = 0; i < _nonce_bytes; ++i)
+		_temp.push_back (dist (_rng));
+
+	xf::HMAC hmac (xf::HMAC::Key (_key), _temp);
+
+	// Add some of the bytes of HMAC signature:
+	size_t hmac_bytes = std::min<size_t> (_signature_bytes, std::distance (hmac.begin(), hmac.end()));
+	_temp.insert (_temp.end(), hmac.begin(), hmac.begin() + hmac_bytes);
+
+	// Output:
+	blob.insert (blob.end(), _temp.begin(), _temp.end());
+}
+
+
+Blob::const_iterator
+LinkProtocol::Signature::eat (Blob::const_iterator begin, Blob::const_iterator end)
+{
+	auto const data_size = Sequence::size();
+	auto const whole_size = size();
+
+	if (std::distance (begin, end) < static_cast<Blob::difference_type> (whole_size))
+		throw InsufficientDataError();
+
+	auto const sign_begin = begin + data_size + _nonce_bytes;
+	auto const sign_end = begin + whole_size;
+
+	// Make a temporary copy of the data:
+	_temp.resize (data_size + _nonce_bytes);
+	std::copy (begin, sign_begin, _temp.begin());
+
+	xf::HMAC hmac (xf::HMAC::Key (_key), _temp);
+
+	// If HMACs differ, that's a parsing error:
+	if (!std::equal (sign_begin, sign_end, hmac.begin()))
+		throw ParseError();
+
+	if (Sequence::eat (begin, begin + data_size) != begin + data_size)
+		throw ParseError();
+
+	return begin + whole_size;
+}
+
+
+LinkProtocol::Envelope::Envelope (Magic magic, PacketList packets):
+	Sequence (packets),
+	_magic (*magic)
+{ }
+
+
+LinkProtocol::Envelope::Envelope (Magic magic, SendEvery send_every, SendOffset send_offset, PacketList packets):
+	Sequence (packets),
+	_magic (*magic),
+	_send_every (*send_every),
+	_send_offset (*send_offset)
+{ }
+
+
+Blob const&
+LinkProtocol::Envelope::magic() const
+{
+	return _magic;
+}
+
+
+Blob::size_type
+LinkProtocol::Envelope::size() const
+{
+	return Sequence::size();
+}
+
+
+void
+LinkProtocol::Envelope::produce (Blob& blob)
+{
+	if (_send_pos % _send_every == _send_offset)
+	{
+		blob.insert (blob.end(), _magic.begin(), _magic.end());
+		Sequence::produce (blob);
+	}
+
+	++_send_pos;
+}
+
+
+LinkProtocol::LinkProtocol (EnvelopeList envelopes):
+	_envelopes (envelopes)
+{
+	if (!_envelopes.empty())
+	{
+		_magic_size = _envelopes[0]->magic().size();
+
+		for (auto const& e: _envelopes)
+		{
+			if (e->magic().size() != _magic_size)
+				throw InvalidMagicSize();
+
+			_envelope_magics[e->magic()] = e;
+		}
+	}
+}
+
+
+void
+LinkProtocol::produce (Blob& blob)
+{
+	for (auto& e: _envelopes)
+		e->produce (blob);
+
+#if XEFIS_LINK_SEND_DEBUG
+	log() << "Send: " << to_string (blob) << std::endl;
+#endif
+}
+
+
+Blob::const_iterator
+LinkProtocol::eat (Blob::const_iterator begin, Blob::const_iterator end, LinkIO* io, QTimer* reacquire_timer, QTimer* failsafe_timer)
+{
+#if XEFIS_LINK_RECV_DEBUG
+	log() << "Recv: " << to_string (Blob (begin, end)) << std::endl;
+#endif
+
+	_aux_magic_buffer.resize (_magic_size);
+
+	while (std::distance (begin, end) > static_cast<Blob::difference_type> (_magic_size + 1))
+	{
+		auto skip_byte_and_retry = [&]() {
+			// Skip one byte and try again:
+			if (std::distance (begin, end) >= 1)
+				++begin;
+
+			if (io)
+				io->link_error_bytes = io->link_error_bytes.value_or (0) + 1;
+
+			// Since there was an error, stop the reacquire timer:
+			if (reacquire_timer)
+				reacquire_timer->stop();
+		};
+
+		bool return_from_outer_function = false;
+		Blob::const_iterator outer_result = begin;
+
+		xf::Exception::guard ([&] {
+			try {
+				// Find the right magic and envelope:
+				std::copy (begin, begin + _magic_size, _aux_magic_buffer.begin());
+				auto envelope_and_magic = _envelope_magics.find (_aux_magic_buffer);
+
+				// If not found, retry starting with next byte:
+				if (envelope_and_magic == _envelope_magics.end())
+					throw LinkProtocol::ParseError();
+
+				auto envelope = envelope_and_magic->second;
+				// Now see if we have enough data in input buffer for this envelope type.
+				// If not, return and retry when enough data is read.
+				if (static_cast<Blob::size_type> (std::distance (begin, end)) - _magic_size < envelope->size())
+				{
+					return_from_outer_function = true;
+					outer_result = begin;
+					return;
+				}
+
+				auto e = envelope->eat (begin + _magic_size, end);
+
+				if (e != begin)
+				{
+					envelope->apply();
+					begin = e;
+				}
+
+				if (io)
+					io->link_valid_envelopes = io->link_valid_envelopes.value_or (0) + 1;
+
+				// Restart failsafe timer:
+				if (failsafe_timer)
+					failsafe_timer->start();
+
+				// If link is not valid, and we got valid envelope,
+				// start reacquire timer:
+				if (reacquire_timer && io)
+					if (!io->link_valid.value_or (false) && !reacquire_timer->isActive())
+						reacquire_timer->start();
+			}
+			catch (LinkProtocol::ParseError&)
+			{
+				skip_byte_and_retry();
+			}
+			catch (...)
+			{
+				skip_byte_and_retry();
+				throw;
+			}
+		});
+
+		if (return_from_outer_function)
+			return outer_result;
+	}
+
+	return begin;
+}
+
+
+Blob::size_type
+LinkProtocol::size() const
+{
+	Blob::size_type s = 0;
+
+	for (auto p: _envelopes)
+		s += p->size();
+
+	return s;
+}
+
+
+void
+LinkProtocol::failsafe()
+{
+	for (auto& e: _envelopes)
+		e->failsafe();
+}
+
+
+std::string
+LinkProtocol::to_string (Blob const& blob)
+{
+	if (blob.empty())
+		return "";
+
+	std::string s;
+
+	for (auto v: blob)
+		s += QString ("%1").arg (v, 2, 16, QChar ('0')).toStdString() + ":";
+
+	s.pop_back();
+	return s;
+}
+
+
+Link::Link (std::unique_ptr<LinkIO> module_io, std::unique_ptr<LinkProtocol> protocol, std::string const& instance):
+	Module (std::move (module_io), instance),
+	_protocol (std::move (protocol))
+{
+	_input_blob.reserve (2 * _protocol->size());
+	_output_blob.reserve (2 * _protocol->size());
+
+	_failsafe_timer = new QTimer (this);
+	_failsafe_timer->setSingleShot (true);
+	_failsafe_timer->setInterval (io.failsafe_after->quantity<Millisecond>());
+	QObject::connect (_failsafe_timer, SIGNAL (timeout()), this, SLOT (failsafe()));
+
+	_reacquire_timer = new QTimer (this);
+	_reacquire_timer->setSingleShot (true);
+	_reacquire_timer->setInterval (io.reacquire_after->quantity<Millisecond>());
+	QObject::connect (_reacquire_timer, SIGNAL (timeout()), this, SLOT (reacquire()));
+
+	_output_timer = new QTimer (this);
+	_output_timer->setSingleShot (false);
+	_output_timer->setInterval (1000_Hz / *io.send_frequency);
+	QObject::connect (_output_timer, SIGNAL (timeout()), this, SLOT (send_output()));
+	_output_timer->start();
+}
+
+
+void
+Link::process (v2::Cycle const&)
+{
+	try {
+		if (io.link_input && _input_changed())
+		{
+			_input_blob.insert (_input_blob.end(), io.link_input->begin(), io.link_input->end());
+			auto e = _protocol->eat (_input_blob.begin(), _input_blob.end(), &io, _reacquire_timer, _failsafe_timer);
+			auto valid_bytes = std::distance (_input_blob.cbegin(), e);
+			io.link_valid_bytes = io.link_valid_bytes.value_or (0) + valid_bytes;
+			_input_blob.erase (_input_blob.begin(), e);
+		}
+	}
+	catch (LinkProtocol::ParseError const&)
+	{
+		log() << "Packet parse error. Couldn't synchronize." << std::endl;
+	}
+}
+
+
+void
+Link::send_output()
+{
+	_output_blob.clear();
+	_protocol->produce (_output_blob);
+	io.link_output = std::string (_output_blob.begin(), _output_blob.end());
+}
+
+
+void
+Link::failsafe()
+{
+	io.link_valid = false;
+	io.link_failsafes = io.link_failsafes.value_or (0) + 1;
+	_protocol->failsafe();
+}
+
+
+void
+Link::reacquire()
+{
+	io.link_valid = true;
+	io.link_reacquires = io.link_reacquires.value_or (0) + 1;
+}
+
---
+++ src/xefis/modules/io/tests/link.test.cc
@@ -0,0 +1,349 @@
+/* vim:ts=4
+ *
+ * Copyleft 2008…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Xefis:
+#include <xefis/test/test.h>
+#include <xefis/modules/io/link.h>
+
+
+namespace xf {
+namespace test {
+
+template<template<class> class PropertyType>
+	class GCS2AircraftLinkIO: public LinkIO
+	{
+	  public:
+		PropertyType<si::Angle>		nil_prop				{ this, "/nil" };
+		PropertyType<si::Angle>		angle_prop				{ this, "/angle" };
+		PropertyType<si::Angle>		angle_prop_r			{ this, "/angle_r" };
+		PropertyType<si::Velocity>	velocity_prop			{ this, "/velocity" };
+		PropertyType<si::Velocity>	velocity_prop_r			{ this, "/velocity_r" };
+		PropertyType<si::Velocity>	velocity_prop_offset	{ this, "/velocity_prop_offset" };
+		PropertyType<si::Velocity>	velocity_prop_offset_r	{ this, "/velocity_prop_offset_r" };
+		PropertyType<bool>			bool_prop				{ this, "/bool" };
+		PropertyType<bool>			bool_prop_r				{ this, "/bool_r" };
+		PropertyType<int64_t>		int_prop				{ this, "/int" };
+		PropertyType<int64_t>		int_prop_r				{ this, "/int_r" };
+		PropertyType<uint64_t>		uint_prop				{ this, "/uint" };
+		PropertyType<uint64_t>		uint_prop_r				{ this, "/uint_r" };
+		PropertyType<int64_t>		dummy					{ this, "/dummy" };
+	};
+
+
+using GCS_Tx_LinkIO = GCS2AircraftLinkIO<v2::PropertyIn>;
+using Aircraft_Rx_LinkIO = GCS2AircraftLinkIO<v2::PropertyOut>;
+
+
+constexpr auto kFallbackBool	= true;
+constexpr auto kFallbackInt		= 12UL;
+
+
+class GCS_Tx_LinkProtocol: public LinkProtocol
+{
+  public:
+	// Ctor
+	template<class IO>
+		explicit
+		GCS_Tx_LinkProtocol (IO* io):
+			LinkProtocol ({
+				envelope (Magic ({ 0xe4, 0x40 }), {
+					signature (NonceBytes (8), SignatureBytes (12), Key ({ 0x88, 0x99, 0xaa, 0xbb }), {
+						property<8> (io->nil_prop,					Retained (false)),
+						property<8> (io->angle_prop,				Retained (false)),
+						property<8> (io->angle_prop_r,				Retained (true)),
+						property<2> (io->velocity_prop,				Retained (false)),
+						property<2> (io->velocity_prop_r,			Retained (true)),
+						property<2> (io->velocity_prop_offset,		Retained (false),	1000_kph),
+						property<2> (io->velocity_prop_offset_r,	Retained (true),	1000_kph),
+						property<2> (io->int_prop,					Retained (false),	0L),
+						property<2> (io->int_prop_r,				Retained (true),	0L),
+					}),
+				}),
+				envelope (Magic ({ 0xa3, 0x80 }), {
+					signature (NonceBytes (8), SignatureBytes (8), Key ({ 0x55, 0x37, 0x12, 0xf9 }), {
+						bitfield ({
+							bitfield_property (io->bool_prop,					Retained (false),	kFallbackBool),
+							bitfield_property (io->bool_prop_r,					Retained (true),	kFallbackBool),
+							bitfield_property (io->uint_prop,		Bits (4),	Retained (true),	kFallbackInt),
+							bitfield_property (io->uint_prop_r,		Bits (4),	Retained (true),	kFallbackInt),
+						}),
+					}),
+				}),
+				envelope (Magic ({ 0x01, 0x02 }), SendEvery (10), SendOffset (8), {
+					property<4> (io->dummy, Retained (false), 0L),
+				}),
+			})
+		{ }
+};
+
+
+class GCS_Rx_LinkProtocol: public LinkProtocol
+{
+  public:
+	// Ctor
+	template<class IO>
+		explicit
+		GCS_Rx_LinkProtocol (IO* io):
+			LinkProtocol({
+				envelope (Magic ({ 0xe4, 0x40 }), {
+					signature (NonceBytes (8), SignatureBytes (12), Key ({ 0x87, 0x11, 0x65, 0xa4 }), {
+					}),
+				}),
+			})
+		{ }
+};
+
+
+void transmit (LinkProtocol& tx_protocol, LinkProtocol& rx_protocol)
+{
+	Blob blob;
+	tx_protocol.produce (blob);
+	auto end = rx_protocol.eat (blob.begin(), blob.end(), nullptr, nullptr, nullptr);
+
+	xf::test_asserts::verify ("rx_protocol ate all input bytes", end == blob.end());
+}
+
+
+static xf::RuntimeTest t1 ("modules/io/link: protocol: valid data transmission", []{
+	GCS_Tx_LinkIO tx_io;
+	Aircraft_Rx_LinkIO rx_io;
+	GCS_Tx_LinkProtocol tx_protocol (&tx_io);
+	GCS_Tx_LinkProtocol rx_protocol (&rx_io);
+
+	auto test = [&] {
+		transmit (tx_protocol, rx_protocol);
+		xf::test_asserts::verify ("nil_prop transmitted properly", rx_io.angle_prop.get_optional() == tx_io.angle_prop.get_optional());
+		xf::test_asserts::verify ("angle_prop transmitted properly", *rx_io.angle_prop == *tx_io.angle_prop);
+		xf::test_asserts::verify_equal_with_epsilon ("velocity transmitted properly", *rx_io.velocity_prop, *tx_io.velocity_prop, 0.1_kph);
+		xf::test_asserts::verify_equal_with_epsilon ("velocity prop with offset transmitted properly", *rx_io.velocity_prop_offset, *tx_io.velocity_prop_offset, 0.1_mps);
+		xf::test_asserts::verify ("bool_prop transmitted properly", *rx_io.bool_prop == *tx_io.bool_prop);
+		xf::test_asserts::verify ("int_prop transmitted properly", *rx_io.int_prop == *tx_io.int_prop);
+		xf::test_asserts::verify ("uint_prop transmitted properly", *rx_io.uint_prop == *tx_io.uint_prop);
+	};
+
+	tx_io.angle_prop = 1.99_rad;
+	tx_io.velocity_prop = 101_kph;
+	tx_io.velocity_prop_offset = 101_kph;
+	tx_io.bool_prop = true;
+	tx_io.int_prop = -2;
+	tx_io.uint_prop = 3;
+	test();
+
+	for (auto const& angle: { -12_rad, 0_rad, 0.99_rad, 1.59_rad, 300_rad })
+	{
+		tx_io.angle_prop = angle;
+		test();
+	}
+
+	for (auto b: { false, true })
+	{
+		tx_io.bool_prop = b;
+		test();
+	}
+
+	for (auto i: { -9, -7, -5, -3, -2, -1, 0, 1, 2, 3, 5, 7, 9 })
+	{
+		tx_io.int_prop = i;
+		test();
+	}
+
+	for (auto i: { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 })
+	{
+		tx_io.uint_prop = i;
+		test();
+	}
+});
+
+
+static xf::RuntimeTest t2 ("modules/io/link: protocol: nils and out-of range values transmission", []{
+	GCS_Tx_LinkIO tx_io;
+	Aircraft_Rx_LinkIO rx_io;
+	GCS_Tx_LinkProtocol tx_protocol (&tx_io);
+	GCS_Tx_LinkProtocol rx_protocol (&rx_io);
+
+	// Test bit-bool:
+
+	tx_io.bool_prop = true;
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("bit-bool 1 is transmitted properly", *rx_io.bool_prop == *tx_io.bool_prop);
+
+	tx_io.bool_prop = false;
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("bit-bool 0 is transmitted properly", *rx_io.bool_prop == *tx_io.bool_prop);
+
+	tx_io.bool_prop.set_nil();
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("nil bit-bool set to fall-back value", *rx_io.bool_prop == kFallbackBool);
+
+	// Test bit-int:
+
+	tx_io.uint_prop = 11;
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("bit-int 11 transmitted properly", *rx_io.uint_prop == *tx_io.uint_prop);
+
+	tx_io.uint_prop = 17;
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("out-of-range bit-int set to fall-back value", *rx_io.uint_prop == kFallbackInt);
+
+	tx_io.uint_prop = 15;
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("bit-int 15 transmitted properly", *rx_io.uint_prop == *tx_io.uint_prop);
+
+	tx_io.uint_prop.set_nil();
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("nil bit-int set to fall-back value", *rx_io.uint_prop == kFallbackInt);
+});
+
+
+static xf::RuntimeTest t3 ("modules/io/link: protocol: offsets increase precision", []{
+	GCS_Tx_LinkIO tx_io;
+	Aircraft_Rx_LinkIO rx_io;
+	GCS_Tx_LinkProtocol tx_protocol (&tx_io);
+	GCS_Tx_LinkProtocol rx_protocol (&rx_io);
+
+	tx_io.velocity_prop = 1001_kph;
+	tx_io.velocity_prop_offset = 1001_kph;
+	transmit (tx_protocol, rx_protocol);
+	auto delta = si::abs (*rx_io.velocity_prop - *tx_io.velocity_prop);
+	auto delta_with_offset = si::abs (*rx_io.velocity_prop_offset - *tx_io.velocity_prop_offset);
+	xf::test_asserts::verify ("offsets increase precision", delta_with_offset < delta);
+});
+
+
+static xf::RuntimeTest t4 ("modules/io/link: protocol: invalid data transmission (wrong signature)", []{
+	// This tests signature verification and checks if values are retained or not, according to the protocol.
+
+	GCS_Tx_LinkIO tx_io;
+	Aircraft_Rx_LinkIO rx_io;
+	GCS_Tx_LinkProtocol tx_protocol (&tx_io);
+	GCS_Tx_LinkProtocol rx_protocol (&rx_io);
+
+	// Transmit correctly first:
+	tx_io.nil_prop.set_nil();
+	tx_io.angle_prop = 15_rad;
+	tx_io.angle_prop_r = 15_rad;
+	tx_io.velocity_prop = 100_mps;
+	tx_io.velocity_prop_r = 100_mps;
+	tx_io.velocity_prop_offset = 102_mps;
+	tx_io.velocity_prop_offset_r = 102_mps;
+	tx_io.bool_prop = false;
+	tx_io.bool_prop_r = false;
+	tx_io.int_prop = -5;
+	tx_io.int_prop_r = -5;
+	tx_io.uint_prop = 15;
+	tx_io.uint_prop_r = 15;
+
+	Blob blob;
+	tx_protocol.produce (blob);
+	rx_protocol.eat (blob.begin(), blob.end(), nullptr, nullptr, nullptr);
+
+	// Transmit invalid data:
+	tx_io.nil_prop = 1_rad;
+	tx_io.angle_prop = 16_rad;
+	tx_io.angle_prop_r = 16_rad;
+	tx_io.velocity_prop = 101_mps;
+	tx_io.velocity_prop_r = 101_mps;
+	tx_io.velocity_prop_offset = 103_mps;
+	tx_io.velocity_prop_offset_r = 103_mps;
+	tx_io.bool_prop = true;
+	tx_io.bool_prop_r = true;
+	tx_io.int_prop = -3;
+	tx_io.int_prop_r = -3;
+	tx_io.uint_prop = 12;
+	tx_io.uint_prop_r = 12;
+
+	blob.clear();
+	tx_protocol.produce (blob);
+	xf::test_asserts::verify ("blob is long enough", blob.size() >= 16);
+	// Mess with both messages:
+	blob[12] = 0x00;
+	blob[13] = 0xff;
+	blob[14] = 0x00;
+	blob[15] = 0xff;
+	// Careful not to touch the last envelope, which shouldn't be sent in second 
+	blob[blob.size() - 6] = 0xff;
+	blob[blob.size() - 5] = 0x00;
+	blob[blob.size() - 4] = 0xff;
+	rx_protocol.eat (blob.begin(), blob.end(), nullptr, nullptr, nullptr);
+
+	// Test that values weren't changed during last invalid transmission:
+	xf::test_asserts::verify ("nil_prop didn't change", !rx_io.nil_prop);
+	xf::test_asserts::verify ("angle_prop is didn't change", *rx_io.angle_prop == 15_rad);
+	xf::test_asserts::verify_equal_with_epsilon ("velocity_prop didn't change", *rx_io.velocity_prop, 100_mps, 0.1_mps);
+	xf::test_asserts::verify_equal_with_epsilon ("velocity_prop_offset didn't change", *rx_io.velocity_prop_offset, 102_mps, 0.1_mps);
+	xf::test_asserts::verify ("int_prop didn't change", *rx_io.int_prop == -5);
+	xf::test_asserts::verify ("bool_prop didn't change", *rx_io.bool_prop == false);
+	xf::test_asserts::verify ("uint_prop didn't change", *rx_io.uint_prop == 15);
+
+	// Test fail-safe and retaining of original values:
+	rx_protocol.failsafe();
+	xf::test_asserts::verify ("nil_prop is nil", !rx_io.nil_prop);
+	xf::test_asserts::verify ("angle_prop is nil", !rx_io.angle_prop);
+	xf::test_asserts::verify ("angle_prop_r is retained", *rx_io.angle_prop_r == 15_rad);
+	xf::test_asserts::verify ("velocity_prop is nil", !rx_io.velocity_prop);
+	xf::test_asserts::verify_equal_with_epsilon ("velocity_prop_r is retained", *rx_io.velocity_prop_r, 100_mps, 0.1_mps);
+	xf::test_asserts::verify ("velocity_prop_offset is nil", !rx_io.velocity_prop_offset);
+	xf::test_asserts::verify_equal_with_epsilon ("velocity_prop_offset_r is retained", *rx_io.velocity_prop_offset_r, 102_mps, 0.1_mps);
+	xf::test_asserts::verify ("bool_prop is nil", !rx_io.bool_prop);
+	xf::test_asserts::verify ("bool_prop_r is retained", *rx_io.bool_prop_r == false);
+	xf::test_asserts::verify ("int_prop is nil", !rx_io.int_prop);
+	xf::test_asserts::verify ("int_prop_r is retained", *rx_io.int_prop_r == -5);
+	xf::test_asserts::verify ("uint_prop is nil", !!rx_io.uint_prop);
+	xf::test_asserts::verify ("uint_prop_r is retained", *rx_io.uint_prop_r == 15);
+});
+
+
+static xf::RuntimeTest t5 ("modules/io/link: protocol: send-every/send-offset works", []{
+	// The third envelope should be sent every two packets, starting from packed with index 1.
+
+	GCS_Tx_LinkIO tx_io;
+	Aircraft_Rx_LinkIO rx_io;
+	GCS_Tx_LinkProtocol tx_protocol (&tx_io);
+	GCS_Tx_LinkProtocol rx_protocol (&rx_io);
+
+	constexpr int64_t kFirstInt = 11223344;
+	constexpr int64_t kSecondInt = 66775544;
+
+	tx_io.dummy = kFirstInt;
+	// According to envelope settings, one need to transmit 7 times before the envelope will be
+	// actually sent:
+	for (size_t i = 0; i < 8; ++i)
+	{
+		transmit (tx_protocol, rx_protocol);
+		xf::test_asserts::verify ("last envelope not sent in " + std::to_string (i) + "-th transmission", !rx_io.dummy);
+	}
+
+	// 8th transmission (every 10th envelope is sent, but offset is 8 and this is the first time):
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("last envelope sent for the first time", *rx_io.dummy == *tx_io.dummy);
+
+	tx_io.dummy = kSecondInt;
+
+	// Next 9 transmissions should not send last envelope (it's sent every 10th):
+	for (size_t i = 0; i < 9; ++i)
+	{
+		transmit (tx_protocol, rx_protocol);
+		xf::test_asserts::verify ("last envelope not sent in subsequent transmissions", *rx_io.dummy == kFirstInt);
+	}
+
+	// 10th transmission updates the property:
+	transmit (tx_protocol, rx_protocol);
+	xf::test_asserts::verify ("last envelope sent for the second time", *rx_io.dummy == kSecondInt);
+});
+
+} // namespace test
+} // namespace xf
+
