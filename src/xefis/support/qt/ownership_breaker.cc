df69cc28026cbf3f8445433bb582ffdf88a1fd39
Michał Gawron <michal@gawron.name>
2018-03-01 08:06:46 +0100

    Move stuff around

---
+++ src/xefis/support/qt/ownership_breaker.cc
@@ -0,0 +1,45 @@
+/* vim:ts=4
+ *
+ * Copyleft 2008…2018  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Qt:
+#include <QtWidgets/QLayout>
+
+// Local:
+#include "ownership_breaker.h"
+
+
+namespace xf {
+
+OwnershipBreaker::OwnershipBreaker (QWidget* child, QWidget* parent):
+	QWidget (parent),
+	_child (child)
+{
+	QHBoxLayout* layout = new QHBoxLayout (this);
+	layout->setMargin (0);
+	layout->setSpacing (0);
+	layout->addWidget (child, 0, Qt::AlignTop | Qt::AlignLeft);
+	layout->addItem (new QSpacerItem (0, 0, QSizePolicy::Expanding, QSizePolicy::Expanding));
+}
+
+
+OwnershipBreaker::~OwnershipBreaker()
+{
+	_child->hide();
+	_child->setParent (nullptr);
+}
+
+} // namespace xf
+
