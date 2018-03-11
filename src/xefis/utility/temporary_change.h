a1690fa982e2f85dd4eac51c92c9c414609e03ff
Michał Gawron <michal@gawron.name>
2018-03-11 11:25:08 +0100

    Add TemporaryChange utility

---
+++ src/xefis/utility/temporary_change.h
@@ -0,0 +1,104 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__UTILITY__TEMPORARY_CHANGE_H__INCLUDED
+#define XEFIS__UTILITY__TEMPORARY_CHANGE_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <utility>
+
+// Xefis:
+#include <xefis/config/all.h>
+
+
+namespace xf {
+
+/**
+ * Sets a new value to given object. Restores its original value
+ * upon destruction.
+ */
+template<class pValue, class pNewValue>
+	class TemporaryChange
+	{
+		using Value		= pValue;
+		using NewValue	= pNewValue;
+
+	  public:
+		/**
+		 * \param	value
+		 *			Value to modify. Must live longer than this object.
+		 */
+		explicit
+		TemporaryChange (Value& value, NewValue&& new_value);
+
+		/**
+		 * \param	value
+		 *			Value to modify. Must live longer than this object.
+		 */
+		explicit
+		TemporaryChange (Value& value, NewValue const& new_value);
+
+		// Dtor
+		~TemporaryChange();
+
+		/**
+		 * Explicitly restore original value.
+		 */
+		void
+		restore();
+
+	  private:
+		bool	_modified	{ true };
+		Value&	_value;
+		Value	_original_value;
+	};
+
+
+template<class V, class N>
+	inline
+	TemporaryChange<V, N>::TemporaryChange (Value& value, NewValue&& new_value):
+		_original_value (std::exchange (value, new_value))
+	{ }
+
+
+template<class V, class N>
+	inline
+	TemporaryChange<V, N>::TemporaryChange (Value& value, NewValue const& new_value):
+		_original_value (std::exchange (value, new_value))
+	{ }
+
+
+template<class V, class N>
+	inline
+	TemporaryChange<V, N>::~TemporaryChange()
+	{
+		restore();
+	}
+
+
+template<class V, class N>
+	inline void
+	TemporaryChange<V, N>::restore()
+	{
+		if (_modified)
+		{
+			_value = std::move (_original_value);
+			_modified = false;
+		}
+	}
+
+} // namespace xf
+
+#endif
+
