04d524bd9d9f90473b7f89c71fc8121eb27e1e6e
Michał Gawron <michal@gawron.name>
2018-04-13 08:11:06 +0200

    More InstrumentAids utilities

--- src/xefis/support/instrument/instrument_aids.h
+++ src/xefis/support/instrument/instrument_aids.h
@@ -55,9 +55,15 @@ class InstrumentAids
 		get_digit_height (QFont const& font);
 	};
 
-	static constexpr char			DIGITS[10]			= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	static constexpr char32_t		MINUS_SIGN			= U'−';
-	static constexpr char const*	MINUS_SIGN_STR_UTF8	= "−";
+	static constexpr char			DIGITS[10]				{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	static constexpr char32_t		MINUS_SIGN				{ U'−' };
+	static constexpr char const*	MINUS_SIGN_STR_UTF8		{ "−" };
+	static constexpr QColor			kAutopilotColor			{ 250, 20, 255 };
+	static constexpr QColor			kNavigationColor		{ 60, 255, 40 };
+	static constexpr QColor			kCautionColor			{ 255, 200, 50 };
+	static constexpr QColor			kWarningColor			{ 255, 40, 40 };
+	static constexpr QColor			kSilver					{ 0xcc, 0xca, 0xc2 };
+	static constexpr QColor			kCyan					{ 0x00, 0xcc, 0xff };
 
   public:
 	// Ctor
@@ -71,12 +77,11 @@ class InstrumentAids
 	InstrumentPainter
 	get_painter (QPaintDevice&) const;
 
-  protected:
-	// TODO initialize these:
-	QFont	font_0;
-	QFont	font_1;
-	QFont	font_2;
-	QFont	font_3;
+	/**
+	 * Return font modified to have given xf::FontSize.
+	 */
+	static QFont
+	resized (QFont const&, xf::FontSize);
 
 	/**
 	 * Return a centered rect inside whole_area that hold given
@@ -97,11 +102,38 @@ class InstrumentAids
 	static float
 	angle_for_qpainter (Angle deg);
 
+	/**
+	 * Return value to use as pen width.
+	 */
+	float
+	pen_width (float scale) const;
+
+	/**
+	 * Return value to use as font pixel size.
+	 */
+	float
+	font_pixel_size (float scale) const;
+
+	/**
+	 * Return pen suitable for instrument drawing.
+	 */
+	QPen
+	get_pen (QColor const& color, float width, Qt::PenStyle style = Qt::SolidLine, Qt::PenCapStyle cap = Qt::RoundCap, Qt::PenJoinStyle join = Qt::MiterJoin) const;
+
   private:
 	QFont							_default_font;
-	// FIXME this implies thread-safety:
-	TextPainter::Cache mutable		_text_painter_cache;
+	TextPainter::Cache mutable		_text_painter_cache; // FIXME this implies thread-safety:
 	std::optional<WidthForHeight>	_aspect_ratio;
+
+  protected:
+	FontInfo	font_0;
+	FontInfo	font_1;
+	FontInfo	font_2;
+	FontInfo	font_3;
+	FontInfo	font_4;
+	FontInfo	font_5;
+	QPen		autopilot_pen_1;
+	QPen		autopilot_pen_2;
 };
 
 
@@ -123,6 +155,29 @@ inline float
 InstrumentAids::angle_for_qpainter (Angle deg)
 {
 	return 16 * deg.in<Degree>();
+}
+
+
+inline float
+InstrumentAids::pen_width (float scale) const
+{
+	return std::max (0.0f, 1.66f * scale * _master_pen_scale);
+}
+
+
+inline float
+InstrumentAids::font_pixel_size (float scale) const
+{
+	return std::max (1.0f, 1.26f * _master_font_scale * scale);
+}
+
+
+inline QPen
+InstrumentAids::get_pen (QColor const& color, float width, Qt::PenStyle style, Qt::PenCapStyle cap, Qt::PenJoinStyle join) const
+{
+	QPen pen { color, pen_width (width), style, cap, join };
+	pen.setMiterLimit (0.25);
+	return pen;
 }
 
 } // namespace xf
