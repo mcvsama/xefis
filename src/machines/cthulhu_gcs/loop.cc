53730a5a72264d3bf2b7c70f7abb507e819e0a78
Michał Gawron <michal@gawron.name>
2018-07-01 19:53:52 +0200

    Show ADI and generate random data for it

--- src/machines/cthulhu_gcs/loop.cc
+++ src/machines/cthulhu_gcs/loop.cc
@@ -19,6 +19,7 @@
 // Xefis:
 #include <xefis/config/all.h>
 #include <xefis/utility/qdom.h>
+#include <xefis/utility/qutils.h>
 
 // Local:
 #include "loop.h"
@@ -64,6 +65,8 @@ Loop::Loop (xf::Machine* machine, xf::Xefis*):
 	auto link_io_rx = std::make_unique<CthulhuGCS_Rx_LinkIO>();
 	auto link_protocol_rx = std::make_unique<CthulhuGCS_Rx_LinkProtocol> (*link_io_rx);
 
+	auto test_generator_io = std::make_unique<TestGeneratorIO>();
+
 	link_io_tx->send_frequency = 100_Hz;
 
 	link_io_rx->reacquire_after = 150_ms;
@@ -72,17 +75,6 @@ Loop::Loop (xf::Machine* machine, xf::Xefis*):
 	auto adc_io = std::make_unique<AirDataComputerIO>();
 	adc_io->ias_valid_minimum = 30_kt;
 	adc_io->ias_valid_maximum = 900_kt;
-
-	this->joystick_input = load_module<WarthogStick> (std::move (joystick_io), joystick_config, _logger, "stick");
-	this->throttle_input = load_module<JoystickInput> (std::move (throttle_io), throttle_config, _logger, "throttle");
-	this->pedals_input = load_module<JoystickInput> (std::move (pedals_io), pedals_config, _logger, "pedals");
-
-	this->link_tx = load_module<Link> (std::move (link_io_tx), std::move (link_protocol_tx), _logger, "link-tx");
-	this->link_rx = load_module<Link> (std::move (link_io_rx), std::move (link_protocol_rx), _logger, "link-rx");
-
-#if 0
-	this->adc = load_module<AirDataComputer> (std::move (adc_io), _airframe.get());
-#endif
 
 #if 0
 	auto adi_io = std::make_unique<ADI_IO>();
@@ -121,12 +113,127 @@ Loop::Loop (xf::Machine* machine, xf::Xefis*):
 	//this->horizontal_trim->show();
 #endif
 
-	_pfd_screen = std::make_unique<xf::Screen> (QRect (0, 0, 400, 400), 10_Hz);
-
-	auto some_label_io = std::make_unique<LabelIO>();
-	some_label_io->label = "Nergal i Hela";
-	some_label_io->font_scale = 1.0;
-	some_label_io->color = Qt::white;
+	auto adi_io = std::make_unique<ADI_IO>();
+	// Settings:
+	adi_io->speed_ladder_line_every								= 10;
+	adi_io->speed_ladder_number_every							= 20;
+	adi_io->speed_ladder_extent									= 124;
+	adi_io->speed_ladder_minimum								= 20;
+	adi_io->speed_ladder_maximum								= 350;
+	adi_io->altitude_ladder_line_every							= 100;
+	adi_io->altitude_ladder_number_every						= 200;
+	adi_io->altitude_ladder_emphasis_every						= 1000;
+	adi_io->altitude_ladder_bold_every							= 500;
+	adi_io->altitude_ladder_extent								= 825;
+	adi_io->altitude_landing_warning_hi							= 1000_ft;
+	adi_io->altitude_landing_warning_lo							= 500_ft;
+	adi_io->raising_runway_visibility							= 1000_ft; // TODO should only appear in landing mode
+	adi_io->raising_runway_threshold							= 250_ft;
+	adi_io->aoa_visibility_threshold							= 17.5_deg;
+	adi_io->show_mach_above										= 0.4;
+	adi_io->power_eq_1000_fpm									= 1000_W;
+	// IO:
+	adi_io->weight_on_wheels									<< xf::ConstantSource (false);
+	adi_io->speed_ias_serviceable								<< xf::ConstantSource (true);
+	adi_io->speed_ias											<< test_generator_io->create_property<si::Velocity> ("/test/speed/ias", 0_kt, { 0_kt, 300_kt }, 10_kt / 1_s);
+	adi_io->speed_ias_lookahead									<< test_generator_io->create_property<si::Velocity> ("/test/speed/ias.lookahead", 25_kt, { 0_kt, 300_kt }, 8_kt / 1_s);
+	adi_io->speed_ias_minimum									<< test_generator_io->create_property<si::Velocity> ("/speed/ias.minimum", 60_kt, { 50_kt, 70_kt }, 3_kt / 1_s);
+	adi_io->speed_ias_minimum_maneuver							<< test_generator_io->create_property<si::Velocity> ("/speed/ias.minimum.maneuver", 65_kt, { 55_kt, 72_kt }, 3_kt / 1_s);
+	adi_io->speed_ias_maximum_maneuver							<< test_generator_io->create_property<si::Velocity> ("/speed/ias.maximum.maneuver", 245_kt, { 238_kt, 245_kt }, 3_kt / 1_s);
+	adi_io->speed_ias_maximum									<< test_generator_io->create_property<si::Velocity> ("/speed/ias.maximum", 250_kt, { 240_kt, 260_kt }, 3_kt / 1_s);
+	adi_io->speed_mach											<< test_generator_io->create_property<double> ("/speed/mach", 0.0f, { 0.0f, 0.85f }, 0.025f / 1_s);
+	adi_io->speed_ground										<< test_generator_io->create_property<si::Velocity> ("/speed/ground-speed", 0_kt, { 0_kt, 400_kt }, 13_kt / 1_s);
+	adi_io->speed_v1											<< test_generator_io->create_property<si::Velocity> ("/test/speed-bugs/v1", 80_kt, { 78_kt, 82_kt }, 1_kt / 1_s);
+	adi_io->speed_vr											<< test_generator_io->create_property<si::Velocity> ("/test/speed-bugs/vr", 88_kt, { 86_kt, 89_kt }, 1_kt / 1_s);
+	adi_io->speed_vref											<< test_generator_io->create_property<si::Velocity> ("/test/speed-bugs/vref", 95_kt, { 94_kt, 96_kt }, 0.1_kt / 1_s);
+	adi_io->speed_flaps_up_label								<< xf::ConstantSource<std::string> ("UP");
+	adi_io->speed_flaps_up_speed								<< xf::ConstantSource (140_kt);
+	adi_io->speed_flaps_a_label									<< xf::ConstantSource<std::string> ("1"); // TODO _b_ and _a_? maybe "upper" and "lower"?
+	adi_io->speed_flaps_a_speed									<< xf::ConstantSource (120_kt);
+	adi_io->speed_flaps_b_label									<< xf::ConstantSource<std::string> ("5");
+	adi_io->speed_flaps_b_speed									<< xf::ConstantSource (110_kt);
+	adi_io->orientation_serviceable								<< xf::ConstantSource (true);
+	adi_io->orientation_pitch									<< test_generator_io->create_property<si::Angle> ("/test/orientation/pitch", 0_deg, { -90_deg, 90_deg }, 8_deg / 1_s);
+	adi_io->orientation_roll									<< test_generator_io->create_property<si::Angle> ("/test/orientation/roll", 0_deg, { -180_deg, +180_deg }, 1.5_deg / 1_s, TestGeneratorIO::BorderCondition::Periodic);
+	adi_io->orientation_heading_magnetic						<< test_generator_io->create_property<si::Angle> ("/test/orientation/heading.magnetic", 0_deg, { 0_deg, 360_deg }, 2_deg / 1_s, TestGeneratorIO::BorderCondition::Periodic);
+	adi_io->orientation_heading_true							<< test_generator_io->create_property<si::Angle> ("/test/orientation/heading.true", 10_deg, { 0_deg, 360_deg }, 2_deg / 1_s, TestGeneratorIO::BorderCondition::Periodic);
+	adi_io->orientation_heading_numbers_visible					<< xf::ConstantSource (true);
+	adi_io->track_lateral_magnetic								<< test_generator_io->create_property<si::Angle> ("/track/lateral.magnetic", 9_deg, { 0_deg, 360_deg }, 22_deg / 1_s, TestGeneratorIO::BorderCondition::Periodic);
+	adi_io->track_lateral_true									<< test_generator_io->create_property<si::Angle> ("/track/lateral.true", 19_deg, { 0_deg, 360_deg }, 22_deg / 1_s, TestGeneratorIO::BorderCondition::Periodic);
+	adi_io->track_vertical										<< test_generator_io->create_property<si::Angle> ("/track/vertical", 0_deg, { -13_deg, 13_deg }, 1_deg / 1_s);
+	adi_io->fpv_visible											<< xf::ConstantSource (true);
+	adi_io->slip_skid											<< test_generator_io->create_property<si::Angle> ("/slip-skid/angle", 0_deg, { -5_deg, 5_deg }, 0.5_deg / 1_s);
+	adi_io->aoa_alpha											<< test_generator_io->create_property<si::Angle> ("/aoa/alpha", 0_deg, { -2_deg, 15_deg }, 1_deg / 1_s);
+	adi_io->aoa_alpha_maximum									<< test_generator_io->create_property<si::Angle> ("/aoa/alpha.maximum", 13_deg, { 13_deg, 15_deg }, 0.25_deg / 1_s);
+	adi_io->aoa_alpha_visible									<< xf::ConstantSource (true);
+	adi_io->altitude_amsl_serviceable							<< xf::ConstantSource (true);
+	adi_io->altitude_amsl										<< test_generator_io->create_property<si::Length> ("/altitude/amsl", -200_ft, { -200_ft, 2000_ft }, 2000_ft / 1_min);
+	adi_io->altitude_amsl_lookahead								<< test_generator_io->create_property<si::Length> ("/altitude/amsl.lookahead", 10_ft, { 0_ft, 2000_ft }, 100_ft / 1_min);
+	adi_io->altitude_agl_serviceable							<< xf::ConstantSource (true);
+	adi_io->altitude_agl										<< test_generator_io->create_property<si::Length> ("/altitude/agl", -4_ft, { -4_ft, 30_m }, 100_ft / 1_min);
+	adi_io->altitude_minimums_type								<< xf::ConstantSource ("BARO");
+	adi_io->altitude_minimums_setting							<< xf::ConstantSource (300_ft);
+	adi_io->altitude_minimums_amsl								<< xf::ConstantSource (300_ft);
+	adi_io->altitude_landing_amsl								<< xf::ConstantSource (140_ft);
+	adi_io->vertical_speed_serviceable							<< xf::ConstantSource (true);
+	adi_io->vertical_speed										<< test_generator_io->create_property<si::Velocity> ("/vertical-speed/speed", 0_fpm, { -6000_fpm, +6000_fpm }, 100_fpm / 1_s);
+	adi_io->vertical_speed_energy_variometer					<< test_generator_io->create_property<si::Power> ("/vertical-speed/energy-variometer", 0_W, { -1000_W, +1000_W }, 100_W / 1_s);
+	adi_io->pressure_qnh										<< xf::ConstantSource (1013_hPa);
+	adi_io->pressure_display_hpa								<< xf::ConstantSource (true);
+	adi_io->pressure_use_std									<< xf::ConstantSource (true);
+	// TODO xf::PropertyIn<std::string>	adi_io->flight_director_active_name						{ ... } // "L", "R", "C"(enter)?
+	adi_io->flight_director_serviceable							<< xf::ConstantSource (true);
+	adi_io->flight_director_cmd_visible							<< xf::ConstantSource (true);
+	adi_io->flight_director_cmd_altitude						<< xf::ConstantSource (1000_ft);
+	adi_io->flight_director_cmd_altitude_acquired				<< xf::ConstantSource (false);
+	adi_io->flight_director_cmd_ias								<< xf::ConstantSource (100_kt);
+	adi_io->flight_director_cmd_mach							<< xf::ConstantSource (0.34);
+	adi_io->flight_director_cmd_vertical_speed					<< xf::ConstantSource (1500_fpm);
+	adi_io->flight_director_cmd_fpa								<< xf::ConstantSource (5_deg);
+	adi_io->flight_director_guidance_visible					<< xf::ConstantSource (true);
+	adi_io->flight_director_guidance_pitch						<< xf::ConstantSource (2.5_deg);
+	adi_io->flight_director_guidance_roll						<< xf::ConstantSource (0_deg);
+	adi_io->control_stick_visible								<< xf::ConstantSource (true);
+	adi_io->control_stick_pitch									<< xf::ConstantSource (2_deg);
+	adi_io->control_stick_roll									<< xf::ConstantSource (2_deg);
+	adi_io->navaid_reference_visible							<< xf::ConstantSource (true);
+	adi_io->navaid_course_magnetic								<< xf::ConstantSource (150_deg);
+	adi_io->navaid_type_hint									<< xf::ConstantSource ("VOR");
+	adi_io->navaid_identifier									<< xf::ConstantSource ("WRO");
+	adi_io->navaid_distance										<< xf::ConstantSource (1.5_nmi);
+	adi_io->flight_path_deviation_lateral_serviceable			<< xf::ConstantSource (true);
+	adi_io->flight_path_deviation_lateral_approach				<< test_generator_io->create_property<si::Angle> ("/flight-path-deviation/lateral/approach", 0_deg, { -5_deg, 5_deg }, 1_deg / 1_s);
+	adi_io->flight_path_deviation_lateral_flight_path			<< test_generator_io->create_property<si::Angle> ("/flight-path-deviation/lateral/flight-path", 0_deg, { -5_deg, 5_deg }, 2_deg / 1_s);
+	adi_io->flight_path_deviation_vertical_serviceable			<< xf::ConstantSource (true);
+	adi_io->flight_path_deviation_vertical						<< test_generator_io->create_property<si::Angle> ("/flight-path-deviation/vertical/deviation", 0_deg, { -5_deg, 5_deg }, 1_deg / 1_s);
+	adi_io->flight_path_deviation_vertical_approach				<< test_generator_io->create_property<si::Angle> ("/flight-path-deviation/vertical/approach", 0_deg, { -5_deg, 5_deg }, 2_deg / 1_s);
+	adi_io->flight_path_deviation_vertical_flight_path			<< test_generator_io->create_property<si::Angle> ("/flight-path-deviation/vertical/flight-path", 0_deg, { -5_deg, 5_deg }, 3_deg / 1_s);
+	adi_io->flight_path_deviation_mixed_mode					<< xf::ConstantSource (true);
+	adi_io->flight_mode_hint_visible							<< xf::ConstantSource (true);
+	adi_io->flight_mode_hint									<< xf::ConstantSource ("TEST");
+	adi_io->flight_mode_fma_visible								<< xf::ConstantSource (true);
+	adi_io->flight_mode_fma_speed_hint							<< xf::ConstantSource ("THR REF");
+	adi_io->flight_mode_fma_speed_armed_hint					<< xf::ConstantSource ("SPD");
+	adi_io->flight_mode_fma_lateral_hint						<< xf::ConstantSource ("CMD TRK");
+	adi_io->flight_mode_fma_lateral_armed_hint					<< xf::ConstantSource ("ILS TRK");
+	adi_io->flight_mode_fma_vertical_hint						<< xf::ConstantSource ("CMD FPA");
+	adi_io->flight_mode_fma_vertical_armed_hint					<< xf::ConstantSource ("G/S");
+	adi_io->tcas_resolution_advisory_pitch_minimum				<< xf::ConstantSource (-45_deg);
+	adi_io->tcas_resolution_advisory_pitch_maximum				<< xf::ConstantSource (80_deg);
+	adi_io->tcas_resolution_advisory_vertical_speed_minimum		<< xf::ConstantSource (-3000_fpm);
+	adi_io->tcas_resolution_advisory_vertical_speed_maximum		<< xf::ConstantSource (10000_fpm);
+#if 0
+	adi_io->warning_novspd_flag									<< xf::ConstantSource (true);
+	adi_io->warning_ldgalt_flag									<< xf::ConstantSource (true);
+	adi_io->warning_pitch_disagree								<< xf::ConstantSource (true);
+	adi_io->warning_roll_disagree								<< xf::ConstantSource (true);
+	adi_io->warning_ias_disagree								<< xf::ConstantSource (true);
+	adi_io->warning_altitude_disagree							<< xf::ConstantSource (true);
+	adi_io->warning_roll										<< xf::ConstantSource (true);
+	adi_io->warning_slip_skid									<< xf::ConstantSource (true);
+#endif
+	adi_io->style_old											<< xf::ConstantSource (false);
+	adi_io->style_show_metric									<< xf::ConstantSource (true);
 
 	auto gear_io = std::make_unique<GearIO>();
 	gear_io->requested_down << xf::ConstantSource (true);
@@ -137,10 +244,21 @@ Loop::Loop (xf::Machine* machine, xf::Xefis*):
 	gear_io->right_up << xf::ConstantSource (false);
 	gear_io->right_down << xf::ConstantSource (true);
 
-	// TODO wsparcie do rejestracji instrumentów tak, żeby nie trzeba było osobno trzymać RegistrationProof: patrz src/__registration__.cc
+	auto some_label_io = std::make_unique<LabelIO>();
+	some_label_io->label = "Nergal i Hela";
+	some_label_io->font_scale = 1.0;
+	some_label_io->color = Qt::white;
+
 	// TODO wrapper na Instrumenty, który ma w środku Instrument oraz RegistrationProof i przeciążony operator->(). Tak że w przypadku skasowania
 	// najpierw będzie skasowany RegistrationProof, a potem Instrument. NO CHYBA ŻE NIE OWNUJEMY INSTRUMENTU… :(
 	// Przykładowy kod do tego:
+	// template<class Value>
+	//	class Registrable
+	//	{
+	//		Value								_value;
+	//		xf::RegistrationProof<Value, int>	_proof;
+	//	}
+	//
 	//struct Instrument
 	//{
 	//	void register_in (xf::Registry<Instrument, int> registry)
@@ -193,13 +311,30 @@ Loop::Loop (xf::Machine* machine, xf::Xefis*):
 	//	return 0;
 	//}
 
-	this->some_label = load_module<Label> (std::move (some_label_io), "some label");
-	this->some_label_registration_proof = _pfd_screen->register_instrument (*this->some_label);
-	_pfd_screen->set (*this->some_label, QRect (0, 0, 100, 200));
+	this->joystick_input = load_module<WarthogStick> (std::move (joystick_io), joystick_config, _logger, "stick");
+	this->throttle_input = load_module<JoystickInput> (std::move (throttle_io), throttle_config, _logger, "throttle");
+	this->pedals_input = load_module<JoystickInput> (std::move (pedals_io), pedals_config, _logger, "pedals");
+
+	this->link_tx = load_module<Link> (std::move (link_io_tx), std::move (link_protocol_tx), _logger, "link-tx");
+	this->link_rx = load_module<Link> (std::move (link_io_rx), std::move (link_protocol_rx), _logger, "link-rx");
+
+#if 0
+	this->adc = load_module<AirDataComputer> (std::move (adc_io), _airframe.get());
+#endif
+
+	this->test_generator = load_module<TestGenerator> (std::move (test_generator_io));
+
+	xf::ScreenSpec spec { QRect { 0, 0, 1366, 768 }, 15_in, 30_Hz, 0.22_mm, 2.1_mm };
+	spec.set_scale (1.5f);
+	_pfd_screen = std::make_unique<xf::Screen> (spec);
+
+	this->adi = load_module<ADI> (std::move (adi_io), "adi");
+	this->adi_registration_proof = _pfd_screen->register_instrument (*this->adi);
+	_pfd_screen->set (*this->adi, { 0.0f, 0.0f, 0.5f, 0.62f });
 
 	this->gear = load_module<Gear> (std::move (gear_io), "gear");
 	this->gear_registration_proof = _pfd_screen->register_instrument (*this->gear);
-	_pfd_screen->set (*this->gear, QRect (100, 0, 500, 500));
+	_pfd_screen->set (*this->gear, { 0.5, 0.0f, 0.5f, 1.0f });
 
 	start();
 }
