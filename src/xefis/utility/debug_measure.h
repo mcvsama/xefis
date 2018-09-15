3de8571d6ef9842bde17a7af770bda10ab14564d
Michał Gawron <michal@gawron.name>
2018-09-15 11:35:46 +0200

    Add simple time/performance measurement tools

---
+++ src/xefis/utility/debug_measure.h
@@ -0,0 +1,89 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__UTILITY__DEBUG_MEASURE_H__INCLUDED
+#define XEFIS__UTILITY__DEBUG_MEASURE_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <ostream>
+
+// Xefis:
+#include <xefis/utility/time_helper.h>
+
+
+#define MEASURE(code) do { \
+	auto duration = xf::TimeHelper::measure ([&] { (code); }); \
+	\
+	if (duration > 0.001_ms) \
+	{ \
+		auto num_chars = duration / 1_ms; \
+		std::cout << std::setprecision(6) << std::fixed << duration.in<si::Second>() << " s  " << std::string (num_chars, '#'); \
+		std::cout << std::string (100 - num_chars, '_') << #code << "\n"; \
+	} \
+} while (false)
+
+
+namespace xf::debug {
+
+class Timer
+{
+  public:
+	// Ctor
+	explicit
+	Timer();
+
+	si::Time
+	get();
+
+	si::Time
+	delta();
+
+  private:
+	si::Time	_start_timestamp;
+	si::Time	_last_check			{ 0_s };
+};
+
+
+inline
+Timer::Timer():
+	_start_timestamp (TimeHelper::now())
+{ }
+
+
+inline si::Time
+Timer::get()
+{
+	_last_check = TimeHelper::now();
+	return _last_check - _start_timestamp;
+}
+
+
+inline si::Time
+Timer::delta()
+{
+	auto now = TimeHelper::now();
+	return now - std::exchange (_last_check, now);
+}
+
+
+inline std::ostream&
+operator<< (std::ostream& os, Timer& t)
+{
+	return os << std::setprecision (6) << std::fixed << t.get();
+}
+
+} // namespace xf::debug
+
+#endif
+
