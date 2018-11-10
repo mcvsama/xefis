1c76661d0cee290983f786153b1fff24d6708645
Michał Gawron <michal@gawron.name>
2018-11-10 16:58:03 +0100

    Add utility/normal_variable.h and utility/normal_distribution.h

---
+++ src/xefis/utility/normal_distribution.h
@@ -0,0 +1,88 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__UTILITY__NORMAL_DISTRIBUTION_H__INCLUDED
+#define XEFIS__UTILITY__NORMAL_DISTRIBUTION_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <random>
+
+// Xefis:
+#include <xefis/utility/normal_variable.h>
+
+
+namespace xf {
+
+/**
+ * A wrapper for std::normal_distribution that allows using non-fundamental types as values.
+ */
+template<class pValue>
+	class NormalDistribution
+	{
+	  public:
+		using Value = pValue;
+
+	  public:
+		// Ctor
+		NormalDistribution (Value mean, Value stddev);
+
+		// Ctor
+		NormalDistribution (NormalVariable<Value> const&);
+
+		// Ctor
+		NormalDistribution (NormalDistribution const&) = default;
+
+		// Copy operator
+		NormalDistribution&
+		operator= (NormalDistribution const&) = default;
+
+		void
+		reset()
+			{ _dist.reset(); }
+
+		Value
+		mean() const noexcept
+			{ return static_cast<Value> (1) * _dist.mean(); }
+
+		Value
+		stddev() const noexcept
+			{ return static_cast<Value> (1) * _dist.stddev(); }
+
+		template<class Generator>
+			Value
+			operator() (Generator& g)
+				{ return static_cast<Value> (1) * _dist (g); }
+
+	  private:
+		std::normal_distribution<> _dist;
+	};
+
+
+template<class V>
+	inline
+	NormalDistribution<V>::NormalDistribution (Value mean, Value stddev):
+		_dist (mean.value(), stddev.value())
+	{ }
+
+
+template<class V>
+	inline
+	NormalDistribution<V>::NormalDistribution (NormalVariable<Value> const& var):
+		_dist (var.mean().value(), var.stddev().value())
+	{ }
+
+} // namespace xf
+
+#endif
+
---
+++ src/xefis/utility/normal_variable.h
@@ -0,0 +1,69 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__UTILITY__NORMAL_VARIABLE_H__INCLUDED
+#define XEFIS__UTILITY__NORMAL_VARIABLE_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+
+namespace xf {
+
+/**
+ * A pair of values, mean (expected value) and standard deviation.
+ */
+template<class pValue>
+	class NormalVariable
+	{
+	  public:
+		using Value = pValue;
+
+	  public:
+		// Ctor
+		constexpr
+		NormalVariable (Value mean, Value stddev);
+
+		// Ctor
+		constexpr
+		NormalVariable (NormalVariable const&) = default;
+
+		// Copy operator
+		constexpr NormalVariable&
+		operator= (NormalVariable const&) = default;
+
+		constexpr Value
+		mean() const noexcept
+			{ return _mean; }
+
+		constexpr Value
+		stddev() const noexcept
+			{ return _stddev; }
+
+	  private:
+		Value	_mean	{ };
+		Value	_stddev	{ };
+	};
+
+
+template<class V>
+	constexpr
+	NormalVariable<V>::NormalVariable (Value mean, Value stddev):
+		_mean (mean),
+		_stddev (stddev)
+	{ }
+
+} // namespace xf
+
+#endif
+
