d99814d03f2b5354fadc1aa08f23afc9fe52f8d4
Michał Gawron <michal@gawron.name>
2018-11-11 19:22:05 +0100

    Add VirtualTemperatureSensor

---
+++ src/xefis/modules/simulation/virtual_temperature_sensor.cc
@@ -0,0 +1,52 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/utility/numeric.h>
+
+// Local:
+#include "virtual_temperature_sensor.h"
+
+
+VirtualTemperatureSensor::VirtualTemperatureSensor (xf::sim::FlightSimulation const& flight_simulation,
+													xf::SpaceVector<si::Length, xf::BodyFrame> const& mount_location,
+													std::unique_ptr<VirtualTemperatureSensorIO> module_io,
+													xf::Logger const& logger,
+													std::string_view const& instance):
+	Module (std::move (module_io), instance),
+	_logger (logger.with_scope (std::string (kLoggerScope) + "#" + instance)),
+	_flight_simulation (flight_simulation),
+	_mount_location (mount_location),
+	_noise (*io.noise)
+{ }
+
+
+void
+VirtualTemperatureSensor::process (xf::Cycle const& cycle)
+{
+	auto const atmstate = _flight_simulation.complete_atmosphere_state_at (_mount_location);
+	auto const& air = atmstate.air;
+
+	io.serviceable = true;
+
+	if (_last_measure_time + *io.update_interval < cycle.update_time())
+	{
+		io.temperature = xf::quantized (_noise (_random_generator) + air.temperature, *io.resolution);
+		_last_measure_time = cycle.update_time();
+	}
+}
+
---
+++ src/xefis/modules/simulation/virtual_temperature_sensor.h
@@ -0,0 +1,78 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__SIMULATION__VIRTUAL_TEMPERATURE_SENSOR_H__INCLUDED
+#define XEFIS__MODULES__SIMULATION__VIRTUAL_TEMPERATURE_SENSOR_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+#include <random>
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/module.h>
+#include <xefis/core/property.h>
+#include <xefis/core/setting.h>
+#include <xefis/support/simulation/flight_simulation.h>
+#include <xefis/utility/normal_distribution.h>
+
+
+class VirtualTemperatureSensorIO: public xf::ModuleIO
+{
+  public:
+	/*
+	 * Settings
+	 */
+
+	xf::Setting<si::Time>								update_interval		{ this, "update_interval" };
+	xf::Setting<xf::NormalVariable<si::Temperature>>	noise				{ this, "noise" };
+	xf::Setting<si::Temperature>						resolution			{ this, "resolution" };
+
+	/*
+	 * Output
+	 */
+
+	xf::PropertyOut<bool>								serviceable			{ this, "serviceable" };
+	xf::PropertyOut<si::Temperature>					temperature			{ this, "measured-temperature" };
+};
+
+
+class VirtualTemperatureSensor: public xf::Module<VirtualTemperatureSensorIO>
+{
+  private:
+	static constexpr char kLoggerScope[] { "mod::VirtualTemperatureSensor" };
+
+  public:
+	// Ctor
+	explicit
+	VirtualTemperatureSensor (xf::sim::FlightSimulation const&,
+							  xf::SpaceVector<si::Length, xf::BodyFrame> const& mount_location,
+							  std::unique_ptr<VirtualTemperatureSensorIO>,
+							  xf::Logger const&,
+							  std::string_view const& instance = {});
+
+	// Module API
+	void
+	process (xf::Cycle const&) override;
+
+  private:
+	xf::Logger									_logger;
+	xf::sim::FlightSimulation const&			_flight_simulation;
+	xf::SpaceVector<si::Length, xf::BodyFrame>	_mount_location;
+	// Device's noise:
+	std::default_random_engine					_random_generator;
+	xf::NormalDistribution<si::Temperature>		_noise				{ 0_K, 0_K };
+	si::Time									_last_measure_time	{ 0_s };
+};
+
+#endif
