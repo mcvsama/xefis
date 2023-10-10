ebb17479b3077c63beb847bbf373dd9988013ce5
Michał Gawron <michal@gawron.name>
2023-10-10 22:11:07 +0200

    Add VirtualServoController

---
+++ src/xefis/modules/simulation/virtual_servo_controller.cc
@@ -0,0 +1,59 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Local:
+#include "virtual_servo_controller.h"
+
+// Xefis:
+#include <xefis/config/all.h>
+
+// Standard:
+#include <cstddef>
+
+
+VirtualServoController::VirtualServoController (std::string_view const& instance):
+	Module (instance)
+{ }
+
+
+xf::ModuleIn<si::Angle>&
+VirtualServoController::socket_for (xf::sim::interfaces::AngularServo& servo)
+{
+	return socket_for (servo, std::format ("servo@{:#016x}", reinterpret_cast<uintptr_t> (&servo)));
+}
+
+
+xf::ModuleIn<si::Angle>&
+VirtualServoController::socket_for (xf::sim::interfaces::AngularServo& servo, std::string_view const name)
+{
+	if (auto it = _angular_servo_sockets.find (&servo);
+		it != _angular_servo_sockets.end())
+	{
+		return *it->second;
+	}
+	else
+	{
+		auto& ref = _angular_servo_sockets[&servo] = std::make_unique<xf::ModuleIn<si::Angle>> (this, name);
+		return *ref;
+	}
+}
+
+
+void
+VirtualServoController::process (xf::Cycle const&)
+{
+	for (auto& [servo, socket_ptr]: _angular_servo_sockets)
+		if (*socket_ptr)
+			servo->set_setpoint (**socket_ptr);
+}
+
---
+++ src/xefis/modules/simulation/virtual_servo_controller.h
@@ -0,0 +1,66 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2016  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__MODULES__SIMULATION__VIRTUAL_SERVO_CONTROLLER_H__INCLUDED
+#define XEFIS__MODULES__SIMULATION__VIRTUAL_SERVO_CONTROLLER_H__INCLUDED
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/core/module.h>
+#include <xefis/core/setting.h>
+#include <xefis/core/sockets/module_socket.h>
+#include <xefis/support/simulation/devices/interfaces/angular_servo.h>
+
+// Standard:
+#include <cstddef>
+#include <string_view>
+
+
+namespace si = neutrino::si;
+
+
+/**
+ * Simulates servo PWM generator.
+ * Can couple IO sockets with provided xf::sim::AngularServos to control
+ * the simulated servos.
+ */
+class VirtualServoController: public xf::Module
+{
+  public:
+	// Ctor
+	explicit
+	VirtualServoController (std::string_view const& instance = {});
+
+	/**
+	 * Allocate/return ModuleIn for the servo (named automatically).
+	 */
+	xf::ModuleIn<si::Angle>&
+	socket_for (xf::sim::interfaces::AngularServo&);
+
+	/**
+	 * Allocate/return new ModuleIn for the servo.
+	 */
+	xf::ModuleIn<si::Angle>&
+	socket_for (xf::sim::interfaces::AngularServo&, std::string_view name);
+
+  protected:
+	// Module API
+	void
+	process (xf::Cycle const&) override;
+
+  private:
+	std::map<xf::sim::interfaces::AngularServo*, std::unique_ptr<xf::ModuleIn<si::Angle>>> _angular_servo_sockets;
+};
+
+#endif
+
