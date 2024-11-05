4ae56021fa464f60485a34dad58f76db3aa742f1
Michał Gawron <michal@gawron.name>
2024-11-05 18:08:15 +0100

    Add PrandtlTube

---
+++ src/xefis/support/simulation/devices/prandtl_tube.cc
@@ -0,0 +1,78 @@
+/* vim:ts=4
+ *
+ * Copyleft 2024  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Local:
+#include "prandtl_tube.h"
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/support/earth/air/air.h>
+#include <xefis/support/nature/various_inertia_tensors.h>
+#include <xefis/support/simulation/rigid_body/various_materials.h>
+#include <xefis/support/simulation/rigid_body/various_shapes.h>
+
+// Standard:
+#include <cstddef>
+
+
+namespace xf::sim {
+
+using rigid_body::BodyCOM;
+using rigid_body::WorldSpace;
+
+
+PrandtlTube::PrandtlTube (Atmosphere const& atmosphere, PrandtlTubeParameters const& params):
+	Body (MassMoments<BodyCOM>()),
+	_atmosphere (&atmosphere)
+{
+	auto const material = rigid_body::make_material ({ 0xff, 0xaa, 0x00 });
+	auto shape = rigid_body::make_cylinder_shape ({
+		.length { params.length },
+		.radius { 0.5 * params.diameter },
+		.num_faces { 7 },
+		.with_bottom = true,
+		.with_top = true,
+		.material = material,
+	});
+	set_shape (shape);
+
+	auto const inertia_tensor_at_com = make_centered_solid_cylinder_inertia_tensor<BodyCOM> ({ .mass = params.mass, .radius = 0.5 * params.diameter, .length = params.length });
+	auto const inertia_tensor_at_origin = inertia_tensor_com_to_point (params.mass, inertia_tensor_at_com, SpaceLength<BodyCOM> { 0_m, 0_m, 0.5 * params.length });
+	set_mass_moments (MassMoments<BodyCOM> (params.mass, { 0_m, 0_m, 0.5 * params.length }, inertia_tensor_at_origin));
+
+	// Make X point into the wind:
+	rotate_about_body_origin (xf::y_rotation<WorldSpace> (90_deg));
+}
+
+
+si::Pressure
+PrandtlTube::static_pressure() const
+{
+	// TODO calculate relative wind from atmosphere and own velocities
+	// TODO variations of pressure coming from the relative wind
+	return _atmosphere->air_at (placement().reframe<ECEFSpace, void>().position()).pressure;
+}
+
+
+si::Pressure
+PrandtlTube::total_pressure() const
+{
+	// TODO calculate relative wind from atmosphere and own velocities
+	// TODO variations of pressure coming from the relative wind
+	return xf::total_pressure (*_atmosphere,
+							   placement().reframe<ECEFSpace, ECEFSpace>(),
+							   math::reframe<ECEFSpace, void> (velocity_moments<WorldSpace>().velocity()));
+}
+
+} // namespace xf::sim
+
---
+++ src/xefis/support/simulation/devices/prandtl_tube.h
@@ -0,0 +1,65 @@
+/* vim:ts=4
+ *
+ * Copyleft 2024  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__SUPPORT__SIMULATION__DEVICES__PRANDTL_TUBE_H__INCLUDED
+#define XEFIS__SUPPORT__SIMULATION__DEVICES__PRANDTL_TUBE_H__INCLUDED
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/support/earth/air/atmosphere.h>
+#include <xefis/support/simulation/rigid_body/body.h>
+
+// Standard:
+#include <cstddef>
+
+
+namespace xf::sim {
+
+struct PrandtlTubeParameters
+{
+	si::Mass	mass;
+	si::Length	length;
+	si::Length	diameter;
+};
+
+
+/**
+ * Also called Pitot-static tube, measures both dynamic and static pressures.
+ * The body's X-axis points into the wind (X-axis is sensor's surface normal vector).
+ */
+class PrandtlTube: public rigid_body::Body
+{
+  public:
+	/**
+	 * \param	Atmosphere
+	 *			PrandtlTube can't outlive the Atmosphere.
+	 */
+	explicit
+	PrandtlTube (Atmosphere const&, PrandtlTubeParameters const&);
+
+	[[nodiscard]]
+	si::Pressure
+	static_pressure() const;
+
+	[[nodiscard]]
+	si::Pressure
+	total_pressure() const;
+
+  private:
+	Atmosphere const* _atmosphere;
+};
+
+} // namespace xf::sim
+
+#endif
+
