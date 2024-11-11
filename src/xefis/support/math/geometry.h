c4d002745120eb5f0670ed98ef851fb50abaa99d
Michał Gawron <michal@gawron.name>
2024-11-11 11:26:40 +0100

    (C) update

--- src/xefis/support/math/geometry.h
+++ src/xefis/support/math/geometry.h
@@ -1,6 +1,6 @@
 /* vim:ts=4
  *
- * Copyleft 2012…2018  Michał Gawron
+ * Copyleft 2024  Michał Gawron
  * Marduk Unix Labs, http://mulabs.org/
  *
  * This program is free software: you can redistribute it and/or modify
