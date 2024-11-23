0911f08a6a61930ae904f4d848d6936c125f8f8a
Michał Gawron <michal@gawron.name>
2024-11-23 17:15:16 +0100

    Remove unnecessary #include

--- src/xefis/modules/systems/pc.cc
+++ src/xefis/modules/systems/pc.cc
@@ -17,7 +17,6 @@
 // Xefis:
 #include <xefis/config/all.h>
 #include <xefis/support/airframe/airframe.h>
-#include <xefis/support/airframe/lift.h>
 #include <xefis/support/airframe/types.h>
 #include <xefis/support/earth/air/air.h>
 #include <xefis/support/earth/navigation/wind_triangle.h>
