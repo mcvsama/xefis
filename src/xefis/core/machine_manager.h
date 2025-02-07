279a0be933f26ba7a46f6c1c97ca6eb5fda273a1
Michał Gawron <michal@gawron.name>
2025-02-07 21:13:16 +0100

    ✨ Add machine_manager.h to project sources and define MachineManager class

---
+++ src/xefis/core/machine_manager.h
@@ -0,0 +1,70 @@
+/* vim:ts=4
+ *
+ * Copyleft 2025  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__CORE__MACHINE_MANAGER_H__INCLUDED
+#define XEFIS__CORE__MACHINE_MANAGER_H__INCLUDED
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/app/xefis.h>
+
+// Neutrino:
+#include <neutrino/noncopyable.h>
+
+// Standard:
+#include <cstddef>
+
+
+namespace xf {
+
+/**
+ * This object is created from the main executable.
+ */
+class MachineManager: private Noncopyable
+{
+  public:
+	// Ctor
+	explicit
+	MachineManager (Xefis&);
+
+	// Dtor
+	virtual
+	~MachineManager() = default;
+
+	/**
+	 * Return main Xefis object.
+	 */
+	Xefis&
+	xefis() const noexcept
+		{ return _xefis; }
+
+	/**
+	 * Allocate and return new Machine.
+	 */
+	virtual std::unique_ptr<Machine>
+	make_machine() = 0;
+
+  private:
+	Xefis& _xefis;
+};
+
+
+inline
+MachineManager::MachineManager (Xefis& xefis):
+	_xefis (xefis)
+{ }
+
+} // namespace xf
+
+#endif
+
