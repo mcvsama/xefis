7960afc9921a500b092e1ff68c34f6de1b8fb387
Michał Gawron <michal@gawron.name>
2025-02-16 17:27:32 +0100

    Simplify cartesian() (use sin and cos directly); add overload for just LonLat

--- src/xefis/support/math/geometry.h
+++ src/xefis/support/math/geometry.h
@@ -342,18 +342,23 @@ is_point_2d_inside_triangle_tester (TriangleConcept auto const& triangle)
 
 
 [[nodiscard]]
+inline SpaceVector<double, ECEFSpace>
+cartesian (si::LonLat const& position)
+{
+	auto const cos_lat = cos (position.lat());
+	auto const x = cos_lat * cos (position.lon());
+	auto const y = cos_lat * sin (position.lon());
+	auto const z = sin (position.lat());
+
+	return { x, y, z };
+}
+
+
+[[nodiscard]]
 inline SpaceVector<si::Length, ECEFSpace>
 cartesian (LonLatRadius const& position)
 {
-	auto const r = si::Length (position.radius()).value();
-	auto const wz = std::polar (r, position.lat().in<si::Radian>());
-	auto const xy = std::polar (wz.real(), position.lon().in<si::Radian>());
-
-	return {
-		si::Length (xy.real()),
-		si::Length (xy.imag()),
-		si::Length (wz.imag()),
-	};
+	return position.radius() * cartesian (static_cast<si::LonLat const&> (position));
 }
 
 
