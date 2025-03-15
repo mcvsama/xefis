6879302806d09e67d2c24c6166a8cf8f928962e2
Michał Gawron <michal@gawron.name>
2025-03-15 11:15:09 +0100

    Add debug_value.h for experimenting with parameters at runtime

---
+++ src/xefis/support/debug/debug_value.cc
@@ -0,0 +1,96 @@
+/* vim:ts=4
+ *
+ * Copyleft 2025  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Local:
+#include "debug_value.h"
+
+// Xefis:
+#include <xefis/config/all.h>
+
+// Qt:
+#include <QCheckBox>
+#include <QLabel>
+#include <QWidget>
+
+// Standard:
+#include <cstddef>
+
+
+namespace xf {
+
+QVBoxLayout&
+get_debug_window_layout()
+{
+	static thread_local std::unique_ptr<QWidget> debug_widget;
+
+	if (!debug_widget)
+	{
+		debug_widget = std::make_unique<QWidget>();
+
+		auto* label = new QLabel ("Debug controls");
+		auto font = label->font();
+		font.setPointSize (font.pointSize() * 1.2);
+		label->setFont (font);
+
+		auto* layout = new QVBoxLayout (&*debug_widget);
+		layout->addWidget (label);
+
+		debug_widget->show();
+	}
+
+	return dynamic_cast<QVBoxLayout&> (*debug_widget->layout());
+}
+
+
+bool
+debug_bool (std::string const& name, bool default_value, std::function<void()> const callback)
+{
+	struct Details
+	{
+		QCheckBox*	check_box;
+		bool		value;
+	};
+
+	static std::map<std::string, Details> check_boxes;
+
+	if (auto const found = check_boxes.find (name);
+		found != check_boxes.end())
+	{
+		return found->second.value;
+	}
+	else
+	{
+		auto& details = check_boxes.emplace(
+			std::make_pair (name, Details { new QCheckBox (QString::fromStdString (name)), default_value })
+		).first->second;
+
+		auto& check_box = *details.check_box;
+		check_box.setChecked (details.value);
+
+		QObject::connect (&check_box, &QCheckBox::stateChanged, [&details, callback] (int state) {
+			details.value = state != 0;
+
+			if (callback)
+				callback();
+		});
+
+		auto& window_layout = get_debug_window_layout();
+		window_layout.addWidget (PaintHelper::new_hline());
+		window_layout.addWidget (&check_box);
+
+		return details.value;
+	}
+}
+
+} // namespace xf
+
---
+++ src/xefis/support/debug/debug_value.h
@@ -0,0 +1,127 @@
+/* vim:ts=4
+ *
+ * Copyleft 2025  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef XEFIS__SUPPORT__UI__DEBUG_VALUE_H__INCLUDED
+#define XEFIS__SUPPORT__UI__DEBUG_VALUE_H__INCLUDED
+
+// Xefis:
+#include <xefis/config/all.h>
+#include <xefis/support/ui/paint_helper.h>
+
+// Neutrino:
+#include <neutrino/range.h>
+
+// Qt:
+#include <QSlider>
+#include <QLabel>
+#include <QLayout>
+
+// Standard:
+#include <cstddef>
+#include <memory>
+
+
+namespace xf {
+
+/**
+ * Get layout for the debug window, so that another debug-widget can be added.
+ * If debug window doesn't exist, it's created.
+ */
+QVBoxLayout&
+get_debug_window_layout();
+
+
+[[nodiscard]]
+bool
+debug_bool (std::string const& name, bool default_value = false, std::function<void()> const callback = nullptr);
+
+
+template<class Value>
+	[[nodiscard]]
+	inline Value
+	debug_slider (std::string const& name,
+				  neutrino::Range<Value> const range,
+				  std::optional<Value> default_value,
+				  Value const step = Value (1),
+				  std::function<void()> const callback = nullptr)
+	{
+		struct Details
+		{
+			QSlider*				slider;
+			neutrino::Range<Value>	range;
+			Value					value;
+		};
+
+		static std::map<std::string, Details> sliders;
+
+		if (auto const found = sliders.find (name);
+			found != sliders.end())
+		{
+			return found->second.value;
+		}
+		else
+		{
+			if (!default_value)
+				default_value = range.min();
+
+			auto& details = sliders.emplace(
+				std::make_pair (name, Details { new QSlider (Qt::Horizontal), range, *default_value })
+			).first->second;
+
+			auto& slider = *details.slider;
+			slider.setTickPosition (QSlider::TicksAbove);
+			slider.setTracking (true);
+			auto const page_step = 10 * step;
+			slider.setTickInterval (range.extent() / page_step);
+			slider.setPageStep (range.extent() / page_step);
+			slider.setRange (range.min() / step, range.max() / step);
+			slider.setValue (details.value / step);
+
+			auto ph = PaintHelper (slider);
+			slider.setMinimumWidth (ph.em_pixels_int (25));
+
+			auto* min_label = new QLabel (QString::fromStdString (std::format ("{}", range.min())));
+			min_label->setAlignment (Qt::AlignLeft);
+
+			auto* title_label = new QLabel (QString::fromStdString (std::format ("{}: {}", name, details.value)));
+			title_label->setAlignment (Qt::AlignCenter);
+
+			auto* max_label = new QLabel (QString::fromStdString (std::format ("{}", range.max())));
+			max_label->setAlignment (Qt::AlignRight);
+
+			QObject::connect (&slider, &QSlider::valueChanged, [&details, name, step, title_label, callback] (int value) {
+				details.value = value * step;
+				title_label->setText (QString::fromStdString (std::format ("{}: {}", name, details.value)));
+
+				if (callback)
+					callback();
+			});
+
+			auto* layout = new QGridLayout();
+			layout->addWidget (min_label, 0, 0);
+			layout->addWidget (title_label, 0, 1);
+			layout->addWidget (max_label, 0, 2);
+			layout->addWidget (&slider, 1, 0, 1, 3);
+
+			auto& window_layout = get_debug_window_layout();
+			window_layout.addWidget (PaintHelper::new_hline());
+			window_layout.addLayout (layout);
+
+			return details.value;
+		}
+	}
+
+} // namespace xf
+
+#endif
+
