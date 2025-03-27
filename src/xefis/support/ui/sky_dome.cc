915dd7eaec5e5d843ce0aa66ea48e683c470371e
Michał Gawron <michal@gawron.name>
2025-03-27 19:58:42 +0100

    Sparser mesh for ground

--- src/xefis/support/ui/sky_dome.cc
+++ src/xefis/support/ui/sky_dome.cc
@@ -122,7 +122,7 @@ calculate_ground_slices_and_stacks (si::Angle const horizon_angle, si::Length co
 
 	// Latitude:
 	{
-		auto const n_stacks = 50u;
+		auto const n_stacks = 20u;
 		auto const visible_angle = 90_deg + horizon_angle;
 		auto view_angle = calculate_angle_from_offset_viewpoint (visible_angle, earth_radius, observer_position_radius);
 		auto const delta = (90_deg + view_angle) / n_stacks;
