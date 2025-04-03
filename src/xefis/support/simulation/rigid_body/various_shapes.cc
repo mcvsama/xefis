967f7da2a31dc854c8f5840c72f5b6558ade5dcb
Michał Gawron <michal@gawron.name>
2025-04-03 19:56:52 +0200

    Remove outdated comment

--- src/xefis/support/simulation/rigid_body/various_shapes.cc
+++ src/xefis/support/simulation/rigid_body/various_shapes.cc
@@ -103,7 +103,7 @@ Shape
 make_cube_shape (xf::MassMomentsAtArm<BodyCOM> const& mm, ShapeMaterial const& material)
 {
 	auto shape = make_centered_cube_shape (mm.centered_at_center_of_mass(), material);
-	shape.translate (math::coordinate_system_cast<BodyOrigin, void> (mm.center_of_mass_position())); // FIXME maybe -?
+	shape.translate (math::coordinate_system_cast<BodyOrigin, void> (mm.center_of_mass_position()));
 	return shape;
 }
 
