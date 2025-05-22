18f615b363189593a9e046fc8fea149e281425bd
Michał Gawron <michal@gawron.name>
2025-05-22 16:54:02 +0200

    Make spinboxes CameraControls class members

--- src/xefis/components/simulator/camera_controls.cc
+++ src/xefis/components/simulator/camera_controls.cc
@@ -75,17 +75,17 @@ CameraControls::CameraControls (RigidBodyViewer& viewer, QWidget* parent):
 	auto const ecef_step = 1_m;
 	auto const ecef_decimals = 3;
 
-	auto* const ecef_x = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.x());
-	auto* const ecef_y = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.y());
-	auto* const ecef_z = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.z());
+	_ecef_x = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.x());
+	_ecef_y = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.y());
+	_ecef_z = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->ecef.z());
 
 	auto const polar_step = 1e-3_deg;
 	auto const polar_decimals = 6;
 
 	// Note: order of creation is important for tab-order:
-	auto* const polar_lat = new_spinbox.operator()<si::Quantity<si::Degree>> ({ -90_deg, +90_deg }, polar_step, polar_decimals, coordinates->polar.lat());
-	auto* const polar_lon = new_spinbox.operator()<si::Quantity<si::Degree>> ({ -180_deg, +180_deg }, polar_step, polar_decimals, coordinates->polar.lon());
-	auto* const polar_radius = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->polar.radius());
+	_polar_lat = new_spinbox.operator()<si::Quantity<si::Degree>> ({ -90_deg, +90_deg }, polar_step, polar_decimals, coordinates->polar.lat());
+	_polar_lon = new_spinbox.operator()<si::Quantity<si::Degree>> ({ -180_deg, +180_deg }, polar_step, polar_decimals, coordinates->polar.lon());
+	_polar_radius = new_spinbox.operator()<si::Length> (ecef_range, ecef_step, ecef_decimals, coordinates->polar.radius());
 
 	auto* const cockpit_view = new QRadioButton ("Cockpit view", this);
 	auto* const chase_view = new QRadioButton ("Chase view", this);
@@ -115,9 +115,9 @@ CameraControls::CameraControls (RigidBodyViewer& viewer, QWidget* parent):
 	layout->addWidget (align_right (new QLabel ("Y")), 1, column);
 	layout->addWidget (align_right (new QLabel ("Z")), 2, column);
 	++column;
-	layout->addWidget (ecef_x, 0, column);
-	layout->addWidget (ecef_y, 1, column);
-	layout->addWidget (ecef_z, 2, column);
+	layout->addWidget (_ecef_x, 0, column);
+	layout->addWidget (_ecef_y, 1, column);
+	layout->addWidget (_ecef_z, 2, column);
 	++column;
 	layout->addItem (ph.new_fixed_horizontal_spacer (2.0), 0, column);
 	++column;
@@ -129,9 +129,9 @@ CameraControls::CameraControls (RigidBodyViewer& viewer, QWidget* parent):
 	layout->addWidget (align_right (new QLabel ("Longitude")), 1, column);
 	layout->addWidget (align_right (new QLabel ("Radius")), 2, column);
 	++column;
-	layout->addWidget (polar_lat, 0, column);
-	layout->addWidget (polar_lon, 1, column);
-	layout->addWidget (polar_radius, 2, column);
+	layout->addWidget (_polar_lat, 0, column);
+	layout->addWidget (_polar_lon, 1, column);
+	layout->addWidget (_polar_radius, 2, column);
 	++column;
 	layout->addItem (ph.new_expanding_horizontal_spacer(), 0, column);
 
@@ -142,27 +142,27 @@ CameraControls::CameraControls (RigidBodyViewer& viewer, QWidget* parent):
 		spinbox->setValue (value / Value (1));
 	};
 
-	auto const update_polar_from_ecef = [=] {
+	auto const update_polar_from_ecef = [=, this] {
 		coordinates->polar = to_polar (coordinates->ecef);
-		load_to_spinbox (polar_lon, coordinates->polar.lon().to<si::Degree>());
-		load_to_spinbox (polar_lat, coordinates->polar.lat().to<si::Degree>());
-		load_to_spinbox (polar_radius, coordinates->polar.radius());
+		load_to_spinbox (_polar_lon, coordinates->polar.lon().to<si::Degree>());
+		load_to_spinbox (_polar_lat, coordinates->polar.lat().to<si::Degree>());
+		load_to_spinbox (_polar_radius, coordinates->polar.radius());
 	};
 
-	auto const update_ecef_from_polar = [=] {
+	auto const update_ecef_from_polar = [=, this] {
 		coordinates->ecef = to_cartesian (coordinates->polar);
-		load_to_spinbox (ecef_x, coordinates->ecef.x());
-		load_to_spinbox (ecef_y, coordinates->ecef.y());
-		load_to_spinbox (ecef_z, coordinates->ecef.z());
+		load_to_spinbox (_ecef_x, coordinates->ecef.x());
+		load_to_spinbox (_ecef_y, coordinates->ecef.y());
+		load_to_spinbox (_ecef_z, coordinates->ecef.z());
 	};
 
-	on_value_change (ecef_x, update_polar_from_ecef);
-	on_value_change (ecef_y, update_polar_from_ecef);
-	on_value_change (ecef_z, update_polar_from_ecef);
+	on_value_change (_ecef_x, update_polar_from_ecef);
+	on_value_change (_ecef_y, update_polar_from_ecef);
+	on_value_change (_ecef_z, update_polar_from_ecef);
 
-	on_value_change (polar_lat, update_ecef_from_polar);
-	on_value_change (polar_lon, update_ecef_from_polar);
-	on_value_change (polar_radius, update_ecef_from_polar);
+	on_value_change (_polar_lat, update_ecef_from_polar);
+	on_value_change (_polar_lon, update_ecef_from_polar);
+	on_value_change (_polar_radius, update_ecef_from_polar);
 
 	QObject::connect (cockpit_view, &QRadioButton::clicked, [this](bool) {
 		_rigid_body_viewer.set_camera_mode (RigidBodyPainter::CockpitView);
