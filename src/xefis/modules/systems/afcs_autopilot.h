0c84ff0f73a8d81d5460c9f47782e47bd33ac700
Michał Gawron <michal@gawron.name>
2025-05-25 18:10:59 +0200

    afcs_ap.cc|h → afcs_autopilot.cc|h

--- src/xefis/modules/systems/afcs_autopilot.h
+++ src/xefis/modules/systems/afcs_autopilot.h
@@ -1,6 +1,6 @@
 /* vim:ts=4
  *
- * Copyleft 2012…2016  Michał Gawron
+ * Copyleft 2025  Michał Gawron
  * Marduk Unix Labs, http://mulabs.org/
  *
  * This program is free software: you can redistribute it and/or modify
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef XEFIS__MODULES__SYSTEMS__AFCS_AP_H__INCLUDED
-#define XEFIS__MODULES__SYSTEMS__AFCS_AP_H__INCLUDED
+#ifndef XEFIS__MODULES__SYSTEMS__AFCS_AUTOPILOT_H__INCLUDED
+#define XEFIS__MODULES__SYSTEMS__AFCS_AUTOPILOT_H__INCLUDED
 
 // Xefis:
 #include <xefis/config/all.h>
@@ -34,7 +34,7 @@ namespace si = neutrino::si;
 using namespace neutrino::si::literals;
 
 
-class AFCS_AP_IO: public xf::Module
+class AFCS_Autopilot_IO: public xf::Module
 {
   public:
 	/*
@@ -76,15 +76,15 @@ class AFCS_AP_IO: public xf::Module
 /**
  * Steers control surfaces (ailerons, elevator) to obtain desired orientation (pitch, roll).
  */
-class AFCS_AP: public AFCS_AP_IO
+class AFCS_Autopilot: public AFCS_Autopilot_IO
 {
   private:
-	static constexpr char kLoggerScope[] = "mod::AFCS_AP";
+	static constexpr char kLoggerScope[] = "mod::AFCS_Autopilot";
 
   public:
 	// Ctor
 	explicit
-	AFCS_AP (xf::ProcessingLoop&, xf::Logger const&, std::string_view const& instance = {});
+	AFCS_Autopilot (xf::ProcessingLoop&, xf::Logger const&, std::string_view const& instance = {});
 
   protected:
 	// Module API
@@ -113,7 +113,7 @@ class AFCS_AP: public AFCS_AP_IO
 	diagnose();
 
   private:
-	AFCS_AP_IO&								_io					{ *this };
+	AFCS_Autopilot_IO&						_io					{ *this };
 	xf::Logger								_logger;
 	xf::PIDController<si::Angle, si::Angle>	_elevator_pid;
 	xf::PIDController<si::Angle, si::Angle>	_ailerons_pid;
