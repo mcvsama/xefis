82b04de4857743a7bb56eba31e19b15995c69a80
Michał Gawron <michal@gawron.name>
2025-07-08 21:24:05 +0200

    Style (break long line into two)

--- src/machines/sim-1/aircraft/simulated_aircraft.cc
+++ src/machines/sim-1/aircraft/simulated_aircraft.cc
@@ -221,7 +221,8 @@ make_aircraft (xf::rigid_body::System& rigid_body_system, xf::Atmosphere& simula
 
 	// Prandtl tube
 
-	auto& prandtl_tube = aircraft_group.add<xf::sim::PrandtlTube> (simulated_atmosphere, xf::sim::PrandtlTubeParameters { .mass = 0.1_kg, .length = 20_cm, .diameter = 1_cm }); // TODO mass = 25 g
+	auto prandtl_tube_params = xf::sim::PrandtlTubeParameters { .mass = 0.1_kg, .length = 20_cm, .diameter = 1_cm };
+	auto& prandtl_tube = aircraft_group.add<xf::sim::PrandtlTube> (simulated_atmosphere, prandtl_tube_params); // TODO mass = 25 g
 	prandtl_tube.set_label ("Prandtl tube");
 	prandtl_tube.move_origin_to (wing_l.origin<WorldSpace>() + xf::SpaceLength<WorldSpace> (0_m, -0.75 * main_wing_airfoil.wing_length(), 0_m));
 
