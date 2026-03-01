161d2a2ad36e51a394b855e10314743aa86cf59c
Michał Gawron <michal@gawron.name>
2026-03-01 23:04:23 +0100

    Fix header sentinel in debug_value.h

--- src/xefis/support/debug/debug_value.h
+++ src/xefis/support/debug/debug_value.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef XEFIS__SUPPORT__UI__DEBUG_VALUE_H__INCLUDED
-#define XEFIS__SUPPORT__UI__DEBUG_VALUE_H__INCLUDED
+#ifndef XEFIS__SUPPORT__DEBUG__DEBUG_VALUE_H__INCLUDED
+#define XEFIS__SUPPORT__DEBUG__DEBUG_VALUE_H__INCLUDED
 
 // Xefis:
 #include <xefis/config/all.h>
